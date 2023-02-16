5cebd112e6429d1ad5ac824eded35f5be9f493e5
staszbalazs <stasz.balazs@gmail.com>
2023-02-16 10:00:34 +0100

    Dummy unit test added

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DummyTest {
+
+    @Test
+    public void dummyTest() {
+        int value = 5;
+        assertTrue(value == 5);
+    }
+
+}
