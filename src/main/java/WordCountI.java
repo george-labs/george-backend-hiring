9ec01f1239ce2504be76ac02e2ce66d11f863038
staszbalazs <stasz.balazs@gmail.com>
2023-02-16 13:55:06 +0100

    WIP refactor Word Count 4

--- src/main/java/WordCountI.java
+++ src/main/java/WordCountI.java
@@ -1,9 +1,7 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.URL;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class WordCountI {
@@ -11,11 +9,11 @@ public class WordCountI {
     private Set<String> stopWords;
 
     WordCountI() {
-        populateStopWords();
+        stopWords = IOUtility.populateStopWords(this.getClass());
     }
 
     public WordCountResultDto wordCountI(String inputFileName) {
-        String input = getInput(inputFileName);
+        String input = IOUtility.getInput(inputFileName);
 
         //Validate the input
         validateInput(input);
@@ -39,34 +37,6 @@ public class WordCountI {
         return new WordCountResultDto(filteredInput.size(), uniqueInput.size());
     }
 
-    private String getInput(String inputFileName) {
-        if (inputFileName != null) {
-            return getInputFromFile(inputFileName);
-        }
-
-        //Get a sentence from the user
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter text: ");
-
-        return myObj.nextLine();
-    }
-
-    private String getInputFromFile(String inputFileName) {
-        //TODO refactor later
-        URL resource = this.getClass().getClassLoader().getResource(inputFileName);
-        if (resource != null) {
-            try (BufferedReader br = new BufferedReader(new FileReader(resource.getFile()))) {
-                return br.lines().collect(Collectors.joining(" "));
-            } catch (IOException ex) {
-                //TODO think about testing this branch
-                System.out.println("Cannot read the" + inputFileName + " file!");
-                throw new RuntimeException(ex);
-            }
-        } else {
-            throw new RuntimeException(new FileNotFoundException("The specified file could not be found!"));
-        }
-    }
-
     private void validateInput(String input) {
         if (input == null || input.isBlank()) {
             throw new RuntimeException("The input string is blank!");
@@ -75,16 +45,6 @@ public class WordCountI {
         boolean valid = input.matches("[a-zA-Z ]+");
         if (!valid) {
             throw new RuntimeException("The input string contains invalid character(s)!");
-        }
-    }
-
-    private void populateStopWords() {
-        URL resource = this.getClass().getClassLoader().getResource("stopwords.txt");
-        //TODO think about it
-        try (BufferedReader br = new BufferedReader(new FileReader(resource.getFile()))) {
-            stopWords = br.lines().collect(Collectors.toSet());
-        } catch (IOException ex) {
-            System.out.println("Cannot read the stopwords.txt file!");
         }
     }
 
