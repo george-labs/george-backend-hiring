beeae7e8ce92f416cb8a116f9b34cfa4a6f6050a
Ivan Zykov <ivan.v.zykov@gmail.com>
2023-02-28 10:07:43 +0100

    Add printer to `ConsoleInputReader`

--- src/main/java/wordcount/inputreader/ConsoleInputReader.java
+++ src/main/java/wordcount/inputreader/ConsoleInputReader.java
@@ -1,11 +1,20 @@
 package wordcount.inputreader;
 
+import wordcount.OutputPrinter;
+
 import java.util.Scanner;
 
 public class ConsoleInputReader implements wordcount.InputReader {
 
+    OutputPrinter outputPrinter;
+
+    public ConsoleInputReader(OutputPrinter outputPrinter) {
+        this.outputPrinter = outputPrinter;
+    }
+
     @Override
     public String readInput() {
+        outputPrinter.print("Number of words: ");
         try (Scanner scanner = new Scanner(System.in)) {
             return scanner.nextLine();
         }
