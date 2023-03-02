fe3f3414bff5873b3da1f2fbb8f3786360d699e0
Marian <mr.varga@gmail.com>
2023-03-02 09:59:03 +0100

    prepare structure - part 1

--- src/main/kotlin/KotlinApplication.kt
+++ src/main/kotlin/KotlinApplication.kt
@@ -1,2 +1,12 @@
 class KotlinApplication {
 }
+
+fun countWords(text: String): Int {
+    return 0
+}
+
+fun main() {
+    print("Enter text: ")
+    val count = countWords(readln())
+    println("Number of words: $count")
+}
