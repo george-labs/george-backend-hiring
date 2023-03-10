a0e65798789ccb866908445f4835f272a60a9db1
dr-divago <matteo.pelliccia@gmail.com>
2023-03-10 13:10:06 +0100

    Iteration 3, adding test for parsing input

--- src/test/java/WordCountAppTest.java
+++ src/test/java/WordCountAppTest.java
@@ -7,11 +7,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class WordCountAppTest {
 
     @Test
-    void when_file_argument_then_read_from_file() throws IOException {
+    void when_no_argument_then_read_from_console() throws IOException {
         String[] args = {};
-        String expected = "Usage: WordCountApp <filename>";
-        String actual = WordCountApp.parseArgument(args);
-        assertEquals(expected, actual);
+        WordCountApp wordCountApp = new WordCountApp(args);
+        WordCountApp.RUNNING_TYPE actual = wordCountApp.getRunningType();
+        assertEquals(WordCountApp.RUNNING_TYPE.CONSOLE, actual);
+    }
+
+    @Test
+    void when_file_argument_then_read_from_file() throws IOException {
+        String[] args = {"src/test/resources/mytext.txt"};
+        WordCountApp wordCountApp = new WordCountApp(args);
+        WordCountApp.RUNNING_TYPE actual = wordCountApp.getRunningType();
+        assertEquals(WordCountApp.RUNNING_TYPE.FILE, actual);
     }
 
 }
