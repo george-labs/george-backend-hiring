05d77f60f6e54f076240c06183c246986230b8e2
dr-divago <matteo.pelliccia@gmail.com>
2023-03-10 10:55:27 +0100

    Iteration 2

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,14 +1,20 @@
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class WordCount {
     public Long count(String s) {
+        return count(s, Collections.emptyList());
+    }
+
+    public Long count(String s, List<String> filterList) {
         if (s.isEmpty()) {
             return 0L;
         }
         String[] stringSplitted = s.split("\\s+");
         return Arrays.stream(stringSplitted)
-                .filter(s1 -> !s1.isEmpty() && !containsSymbol(s1))
+                .filter(s1 -> !s1.isEmpty() && !containsSymbol(s1) && !filterList.contains(s1))
                 .count();
     }
 
