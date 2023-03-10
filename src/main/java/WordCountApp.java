7564c342ba8df0cf38a09148e1f6145fb61c1ac8
dr-divago <matteo.pelliccia@gmail.com>
2023-03-10 13:10:06 +0100

    Iteration 3, adding test for parsing input

--- src/main/java/WordCountApp.java
+++ src/main/java/WordCountApp.java
@@ -2,37 +2,50 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class WordCountApp {
-
-    private enum RUNNING_TYPE {
+    enum RUNNING_TYPE {
         FILE,
         CONSOLE
     };
-
     private RUNNING_TYPE runningType;
 
-    public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            Scanner sc = new Scanner(System.in);
-            StopWordReader stopWordReader = new StopWordReader("stopwords.txt");
-            stopWordReader.read();
-            System.out.println("Enter text:");
-            String input = sc.nextLine();
-            WordCount wordCount = new WordCount();
-            System.out.println("Number of words: " + wordCount.count(input, stopWordReader.getWordsToFilterOut()));
+    private String[] args;
+    public WordCountApp(String[] args) {
+        this.args = args;
+        this.runningType = parseArgument(args);
+
+    }
+    public RUNNING_TYPE getRunningType() {
+        return runningType;
+    }
+
+    public void run() throws IOException {
+        if (runningType.equals(RUNNING_TYPE.CONSOLE)) {
+            runConsole();
         }
-        else {
-            StopWordReader stopWordReader = new StopWordReader("stopwords.txt");
-            stopWordReader.read();
-            TextFileReader textFileReader = new TextFileReader();
-            String text = textFileReader.read(args[0]);
-            WordCount wordCount = new WordCount();
-            System.out.println("Number of words: " + wordCount.count(text, stopWordReader.getWordsToFilterOut()));
-        }
+        runFile();
 
     }
 
+    private void runFile() throws IOException {
+        StopWordReader stopWordReader = new StopWordReader("stopwords.txt");
+        stopWordReader.read();
+        TextFileReader textFileReader = new TextFileReader();
+        String text = textFileReader.read(args[0]);
+        WordCount wordCount = new WordCount();
+        System.out.println("Number of words: " + wordCount.count(text, stopWordReader.getWordsToFilterOut()));
+    }
 
-    public static RUNNING_TYPE parseArgument(String[] args) {
+    private void runConsole() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        StopWordReader stopWordReader = new StopWordReader("stopwords.txt");
+        stopWordReader.read();
+        System.out.println("Enter text:");
+        String input = sc.nextLine();
+        WordCount wordCount = new WordCount();
+        System.out.println("Number of words: " + wordCount.count(input, stopWordReader.getWordsToFilterOut()));
+    }
+
+    private static RUNNING_TYPE parseArgument(String[] args) {
         return args.length == 0 ? RUNNING_TYPE.CONSOLE : RUNNING_TYPE.FILE;
     }
 }
