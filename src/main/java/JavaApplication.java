b967d060635a2a07183f476cea8a89d5b6c5dfe9
Mohammad Alsalkini <eng.salkini@hotmail.com>
2023-03-15 09:59:34 +0100

    Add iterate_1_partail

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class JavaApplication {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = input.nextLine();
+
+        if (text == null || text.isEmpty()) {
+            System.out.println("Text is not correct");
+        }
+
+        String[] words = text.split(" ");
+    }
 }
---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DummyTest {
+
+    @Test
+    void shouldDoDummyTest() {
+        int number = 5;
+
+        assertTrue(number == 5);
+    }
+}
