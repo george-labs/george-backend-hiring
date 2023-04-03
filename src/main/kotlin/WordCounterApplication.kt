2ef63315a56d8796dcb0766417fb2aed056959a7
David Siro <siro.david@gmail.com>
2023-04-03 11:21:07 +0200

    support for input via program args

--- src/main/kotlin/WordCounterApplication.kt
+++ src/main/kotlin/WordCounterApplication.kt
@@ -1,20 +1,33 @@
+import java.io.File
 import java.io.InputStreamReader
-import java.io.Reader
-import java.nio.file.Files
-import java.nio.file.Path
 
 fun main(args: Array<String>) {
-    val stopWords = WordCounter::class.java.getResource("stopwords.txt")!!.openStream().use { inputStream ->
-        InputStreamReader(inputStream).use { reader -> reader.readLines() }
-    }.toSet()
-
+    val stopWords = readStopWordsFromClasspath()
     val counter = WordCounter(stopWords)
 
-    print("Enter text: ")
+    val inputLine = if (args.isEmpty()) {
+        readFromStdin()
+    } else {
+        readFromFile(args)
+    }
 
-    val inputLine = readln()
     val count = counter.countWords(inputLine)
 
     println("Number of words: $count")
 
+}
+
+private fun readStopWordsFromClasspath(): Set<String> {
+    val resourcePath = WordCounter::class.java.getResource("stopwords.txt") ?: error("Resource not found")
+    val stopWords = resourcePath.openStream().use { inputStream ->
+        InputStreamReader(inputStream).use { reader -> reader.readLines() }
+    }.toSet()
+    return stopWords
+}
+
+private fun readFromFile(args: Array<String>) = File(args.first()).readText()
+
+private fun readFromStdin(): String {
+    print("Enter text: ")
+    return readln()
 }
--- src/test/kotlin/WordCounterApplicationKtIntegrationTest.kt
+++ src/test/kotlin/WordCounterApplicationKtIntegrationTest.kt
@@ -24,7 +24,7 @@ class WordCounterApplicationKtIntegrationTest {
     }
 
     @Test
-    fun `should count words`() {
+    fun `should count words for input read from stdin`() {
         System.setIn("Hello world I am David".byteInputStream())
 
         val output = ByteArrayOutputStream().use { bos ->
@@ -41,4 +41,24 @@ class WordCounterApplicationKtIntegrationTest {
         assertEquals("Enter text: Number of words: 4\n", output)
 
     }
+
+
+    @Test
+    fun `should count words for input read from file`() {
+        val output = ByteArrayOutputStream().use { bos ->
+            PrintStream(bos).use { outputStream ->
+                System.setOut(outputStream)
+
+                val inputPath = WordCounterApplicationKtIntegrationTest::class.java.getResource("example_input.txt")?.path ?: error("No resource found!")
+                main(arrayOf(inputPath))
+
+                outputStream.flush()
+
+            }
+            bos.toString()
+        }
+        assertEquals("Number of words: 4\n", output)
+
+    }
+
 }
