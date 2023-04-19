b4c6dfdd35f01854c388a9736db070a45061d8a3
PeterCsatlos <csatlos.peterr@gmail.com>
2023-04-19 09:56:29 +0200

    First iteration initial state

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class JavaApplication {
+
+    public static void main(String[] args) {
+        System.out.println("wordcount");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] words = scanner.nextLine().split(" ");
+
+        System.out.println("Number of words: " + words.length);
+    }
 }
--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class JavaApplicationTest {
+
+    @Test
+    public void dummyTest() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void testMain() {
+
+    }
 }
