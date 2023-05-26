94d2e5761c71cc5e1be4d3920bc586f903441964
Shubhankar Choudhari <cshubhaankar@gmail.com>
2023-05-26 10:06:49 +0200

    First session

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class JavaApplication {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter text: ");
+        String inputText = sc.nextLine();
+        String[] inputWords = inputText.split(" ");
+
+        System.out.println("Output: "+ inputWords.length);
+        sc.close();
+    }
 }
--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,2 +1,9 @@
+import org.junit.jupiter.api.Test;
+
 public class JavaApplicationTest {
+
+    @Test
+    public void testApp(){
+
+    }
 }
