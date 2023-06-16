ef00e587b8d750f7aa1109bbf0a8b18f061b9cfc
Nima Ranjbaran <63397933+NRanjbaran@users.noreply.github.com>
2023-06-16 12:23:29 +0330

    Iteration 2, fetching stop words and match with main words count

--- src/main/java/iteration1/WordCounter.java
+++ src/main/java/iteration1/WordCounter.java
@@ -1,14 +1,47 @@
 package iteration1;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class WordCounter {
 
-    public int count(String sentence) {
-        List<String> words = Arrays.asList(sentence.split(" "));
+    private String fileName;
+    private String enCharacters = Pattern.compile("[^a-zA-Z]").toString();
 
-        return words.size() + 1;
+    public WordCounter(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public int count(String sentence) throws IOException {
+        AtomicInteger counter = new AtomicInteger(0);
+        Set<String> stopWords = fetchStopWords();
+        List<String> words = Arrays.stream(sentence.split(" "))
+                .map(word -> word.replaceAll(enCharacters, ""))
+                .collect(Collectors.toList());
+
+        words.forEach(word -> {
+            boolean isStopWordMatch = stopWords.parallelStream()
+                    .anyMatch(word::equals);
+
+            if (!isStopWordMatch) {
+                counter.getAndIncrement();
+            }
+        });
+
+        return counter.get();
+    }
+
+    private Set<String> fetchStopWords() throws IOException {
+        String stopWordSentence = Files.readString(Paths.get(this.fileName));
+
+        return Set.of(stopWordSentence.split(" "));
     }
 
 }
