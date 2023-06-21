9a09c666dc55b264144514a1a62671b3cbbba0d3
Fodor László <www.fodorlaci@gmail.com>
2023-06-21 09:59:45 +0200

    First iteration done

--- docs/assumptions.md
+++ docs/assumptions.md
@@ -1,3 +1,4 @@
 # Assumptions per iteration
 
 ## Iteration 1
+The assumption is that the word is separated by spaces not by any other character.
--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class JavaApplication {
+
+
+    public static void main(String... args) {
+        System.out.print("Enter text: ");
+        Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine();
+        sc.close();
+
+        int wordsWithoutSpecialCharactersCounter = countWords(inputString, "[a-zA-Z]+");
+
+        System.out.println("Number of words: " + wordsWithoutSpecialCharactersCounter);
+    }
+
+    private static int countWords(String inputString, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        String[] inputWords = inputString.split(" ");
+        int wordsWithoutSpecialCharactersCounter = 0;
+        for (String word : inputWords) {
+            Matcher matcher = pattern.matcher(word);
+            if (matcher.matches()) {
+                wordsWithoutSpecialCharactersCounter++;
+            }
+        }
+        return wordsWithoutSpecialCharactersCounter;
+    }
 }
