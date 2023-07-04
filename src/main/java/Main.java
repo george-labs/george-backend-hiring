1deff5f07ffa214fd44bf120e38b44e887f225d5
birsafilip <birsafilip@gmail.com>
2023-07-04 14:19:12 +0200

    Word_Count_I_I_Removing_Exception_From_Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,19 @@
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         StopWords stopWords = new StopWords();
+
         FileReaderText fileReaderText = new FileReaderText();
 
-        try {
-            stopWords.setStopWords(fileReaderText.readingTxtFileConvertingToListOfString("stopwords.txt"));
-        } catch (IOException e) {
-            System.out.println("Application is not working");
-        }
+        stopWords.setStopWords(fileReaderText.readingTxtFileConvertingToListOfString("stopwords.txt"));
+
+        Sentence sentence = new Sentence(stopWords);
 
         System.out.println("Enter text: ");
         Scanner in = new Scanner(System.in);
 
-        Sentence sentence = new Sentence(stopWords);
         String inputSentence = in.nextLine();
 
         System.out.println("Number of words: " + sentence.countingWordsOnlyLetter(inputSentence));
