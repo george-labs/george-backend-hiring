2cd93493dd016b2d86789c82f23fed8ae4219ec3
DanielKorbel <D.a.n15@seznam.cz>
2023-07-19 11:01:55 +0200

    work in progress - iteration 2 * added stop words text file * create method load stop words - to be finished * added filter for stop words

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -1,4 +1,8 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class WordCounter {
 
@@ -8,9 +12,22 @@ public class WordCounter {
         }
 
         String[] words = input.split("\\s");
+        List<String> stopWords = loadStopWords();
         return Arrays.stream(words)
                 .filter(s -> !s.equals(""))
                 .filter(s -> s.matches("\\b[a-zA-Z]*\\b"))
+                .filter(s -> !stopWords.contains(s))
                 .count();
+    }
+
+    private List<String> loadStopWords() {
+        try {
+            Scanner myReader = new Scanner(new FileReader("..\\resources\\stopwords.txt"));
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
