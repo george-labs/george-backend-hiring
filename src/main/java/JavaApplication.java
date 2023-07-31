cd79e82390aab798a9e41167a1d8966895d2fe9f
Shubham Garg <shubhamgarg1718@gmail.com>
2023-07-31 13:11:14 +0530

    Implement sample code initially

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,7 @@
 public class JavaApplication {
+
+    // sample
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class JavaApplicationTest {
+
+
+    @Test
+    public void testAdd() {
+        JavaApplication app = new JavaApplication();
+        int result = app.add(2, 3);
+        assertEquals(5, result);
+    }
 }
