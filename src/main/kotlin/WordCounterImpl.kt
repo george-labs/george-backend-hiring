4886f98b372044d88f2421510681c8a8b15adbf4
Pavel Shatskikh <pavel.shackih@gmail.com>
2023-08-09 14:09:20 +0300

    Impl word counter

--- src/main/kotlin/WordCounterImpl.kt
+++ src/main/kotlin/WordCounterImpl.kt
@@ -4,6 +4,7 @@ class WordCounterImpl(
 
     override fun countWords(input: String): WordCounterResult {
         val words = input.split(" ")
+            .flatMap { splitWords(it) }
             .map { removeSpecialSymbols(it) }
             .filter { alphabetCheck(it) }
             .filter { !stopWordsProvider.isBanned(it) }
@@ -12,6 +13,15 @@ class WordCounterImpl(
             numberOfWords = words.size,
             unique = words.toSet().size,
         )
+    }
+
+    internal fun splitWords(input: String): List<String> {
+        val index = input.indexOf("-")
+        return if (index != -1 && index != input.length - 1) {
+            listOf(input.substring(0, index), input.substring(index + 1, input.length))
+        } else {
+            listOf(input)
+        }
     }
 
     private fun alphabetCheck(input: String): Boolean {
--- src/test/kotlin/WordCounterTest.kt
+++ src/test/kotlin/WordCounterTest.kt
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.util.*
 
 class WordCounterTest {
 
@@ -12,14 +13,14 @@ class WordCounterTest {
     }
 
     private val pairs = listOf(
-        /**"Mary had a little lamb" to WordCounterResult(5, 5),
+        "Mary had a little lamb" to WordCounterResult(5, 5),
         "Hell0, my n4me is Markus" to WordCounterResult(3, 3),
         "Hello, my name is Markus." to WordCounterResult(5, 5),
         "" to WordCounterResult.ZERO,
         "   " to WordCounterResult.ZERO,
         "Mary   had a  little       lamb  " to WordCounterResult(5, 5),
-        "Hell0, my n4me: is Markus." to WordCounterResult(3, 3),**/
-        "Humpty-Dumpty sat on a wall. Humpty-Dumpty had a great fall." to WordCounterResult(9, 7)
+        "Hell0, my n4me: is Markus." to WordCounterResult(3, 3),
+        "Humpty-Dumpty sat on a wall. Humpty-Dumpty had a great fall." to WordCounterResult(12, 9)
     )
 
     @Test
@@ -28,5 +29,12 @@ class WordCounterTest {
             val actual = counter.countWords(item.first)
             assertEquals(item.second, actual, "For input: ${item.first}")
         }
+    }
+
+    @Test
+    fun `split words`() {
+        assertEquals(listOf(""), counter.splitWords(""))
+        assertEquals(listOf("Humpty", "Dumpty"), counter.splitWords("Humpty-Dumpty"))
+        assertEquals(listOf("abc"), counter.splitWords("abc"))
     }
 }
