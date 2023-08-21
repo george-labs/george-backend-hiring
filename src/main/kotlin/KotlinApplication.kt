a13faa307ac7c399a87b06f7bd13161de1111399
Rabin Gaire <rabingaire20@gmail.com>
2023-08-21 10:27:04 +0200

    Add implementation and test for KotlinApplication.wordCount

--- src/main/kotlin/KotlinApplication.kt
+++ src/main/kotlin/KotlinApplication.kt
@@ -1,7 +1,13 @@
 class KotlinApplication {
     companion object {
         fun wordCount(content: String): Int {
-            return 0
+            val words = content.trim().split(" ").map { it.trim() }.filter { it.isNotEmpty() }
+            val regex = """[a-zA-Z]*""".toRegex()
+            var wordCount = 0
+            for (word in words) {
+                wordCount = if (regex.matches(word)) wordCount + 1 else wordCount
+            }
+            return wordCount
         }
     }
 }
@@ -9,4 +15,6 @@ class KotlinApplication {
 fun main() {
     print("Enter text: ")
     val input = readln()
+    val wordCount = KotlinApplication.wordCount(input)
+    println("Number of words: $wordCount")
 }
--- src/test/kotlin/KotlinApplicationTest.kt
+++ src/test/kotlin/KotlinApplicationTest.kt
@@ -5,10 +5,11 @@ import org.junit.jupiter.params.provider.CsvSource
 class KotlinApplicationTest {
     @ParameterizedTest
     @CsvSource(
+        "      word  word     ,2",
         "word word,2",
-        "word. word,1",
+        "word.    word,1",
         "word word3    word,2",
-        "word3word, 0"
+        "word3word  word., 0"
     )
     fun testWordCount(input: String, expected: Int) {
         val got = KotlinApplication.wordCount(input)
