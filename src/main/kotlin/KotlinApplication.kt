e4c99a2bacdfbb1ec85cdf2d1bd030d55d2b7046
Rabin Gaire <rabingaire20@gmail.com>
2023-08-21 14:22:54 +0200

    Handle multiple exceptions on main method

--- src/main/kotlin/KotlinApplication.kt
+++ src/main/kotlin/KotlinApplication.kt
@@ -16,11 +16,14 @@ fun main(args: Array<String>) {
         val content = if (words.isEmpty()) readUserInput() else words.joinToString(separator=" ")
         val (wordCount, uniqueWordCount) = WordCounter.wordCount(content, stopWords)
         println("Number of words: $wordCount, unique: $uniqueWordCount")
-    } catch(e: InvalidNumberOfArgsException) {
-        println(e.message)
-        exitProcess(1)
-    } catch (e: FileNotFoundException) {
-        println(e.message)
-        exitProcess(2)
+    } catch(e: Exception) {
+        when(e) {
+            is InvalidNumberOfArgsException,
+            is FileNotFoundException-> {
+                println(e.message)
+                exitProcess(1)
+            }
+            else -> throw e
+        }
     }
 }
