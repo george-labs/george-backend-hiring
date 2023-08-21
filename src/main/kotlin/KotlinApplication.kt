17608049b6bccbd5f4343e67bc1728ecbc9f1d1a
Rabin Gaire <rabingaire20@gmail.com>
2023-08-21 10:01:37 +0200

    Add test for KotlinApplication.getCount method

--- src/main/kotlin/KotlinApplication.kt
+++ src/main/kotlin/KotlinApplication.kt
@@ -1,2 +1,12 @@
 class KotlinApplication {
+    companion object {
+        fun wordCount(content: String): Int {
+            return 0
+        }
+    }
+}
+
+fun main() {
+    print("Enter text: ")
+    val input = readln()
 }
--- src/test/kotlin/KotlinApplicationTest.kt
+++ src/test/kotlin/KotlinApplicationTest.kt
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
 class KotlinApplicationTest {
+    @ParameterizedTest
+    @CsvSource(
+        "word word,2",
+        "word. word,1",
+        "word word3    word,2",
+        "word3word, 0"
+    )
+    fun testWordCount(input: String, expected: Int) {
+        val got = KotlinApplication.wordCount(input)
+        assert(expected == got) {
+            "wordCount: Expected $expected, Got $got"
+        }
+    }
 }
