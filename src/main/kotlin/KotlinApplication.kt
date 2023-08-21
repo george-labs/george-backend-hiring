2f75afc62b2d959dde978171d792d9e9be6ddb5b
Rabin Gaire <rabingaire20@gmail.com>
2023-08-21 11:01:58 +0200

    Use the file reader interface to read stopwords.txt

--- src/main/kotlin/KotlinApplication.kt
+++ src/main/kotlin/KotlinApplication.kt
@@ -1,20 +1,31 @@
+import kotlin.system.exitProcess
+
 class KotlinApplication {
     companion object {
-        fun wordCount(content: String): Int {
+        fun wordCount(content: String, stopWords: List<String>): Int {
             val words = content.trim().split(" ").map { it.trim() }.filter { it.isNotEmpty() }
             val regex = """[a-zA-Z]*""".toRegex()
             var wordCount = 0
             for (word in words) {
-                wordCount = if (regex.matches(word)) wordCount + 1 else wordCount
+                wordCount = if (!stopWords.contains(word) && regex.matches(word) ) wordCount + 1 else wordCount
             }
             return wordCount
         }
     }
 }
 
-fun main() {
+fun main(args: Array<String>) {
+    if (args.size != 1) {
+        println("Error usage: wordcount <stop words.txt file path>")
+        exitProcess(1)
+    }
+
+    val file: FileReader = ReadFile(args[0])
+    val stopWords = file.readLines()
+
     print("Enter text: ")
     val input = readln()
-    val wordCount = KotlinApplication.wordCount(input)
+
+    val wordCount = KotlinApplication.wordCount(input, stopWords)
     println("Number of words: $wordCount")
 }
--- src/test/kotlin/KotlinApplicationTest.kt
+++ src/test/kotlin/KotlinApplicationTest.kt
@@ -9,10 +9,14 @@ class KotlinApplicationTest {
         "word word,2",
         "word.    word,1",
         "word word3    word,2",
-        "word3word  word., 0"
+        "word3word  word., 0",
+        "there are 3    cats,3",
+        "please turn on the lights.,2",
+        "    the  3lights are turned off and a washing machine.,4"
     )
     fun testWordCount(input: String, expected: Int) {
-        val got = KotlinApplication.wordCount(input)
+        val stopWords = listOf("the","a", "on", "off")
+        val got = KotlinApplication.wordCount(input, stopWords)
         assert(expected == got) {
             "wordCount: Expected $expected, Got $got"
         }
