b34374681fc8f1519f522a935db42a57800b1d97
JozefMikus <jozo.mikus@gmail.com>
2023-09-20 10:08:35 +0200

    init test

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,6 @@
 public class JavaApplication {
+
+    public void methodToTest(){
+        System.out.println("hey");
+    }
 }
--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,2 +1,13 @@
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class JavaApplicationTest {
+
+    @Test
+    void methodToTest() {
+        assertTrue(true);
+    }
+
 }
