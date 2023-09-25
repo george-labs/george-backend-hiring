828632f78f9976ab0267df579da000eabdaf54e3
Tobias Mueller <tobias.mu@protonmail.com>
2023-09-25 09:12:27 +0100

    Session_1 end

--- docs/assumptions.md
+++ docs/assumptions.md
@@ -1,3 +1,7 @@
 # Assumptions per iteration
 
 ## Iteration 1
+
+* No need to support more than Utf-8
+* Delimiter is everything except 'a-z' & 'A-Z'
+* Delimiters are not words
--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class JavaApplication {
+
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.print("Enter text: ");
+        // Enter data using BufferReader
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+        String input = null;
+        try{
+            input = reader.readLine();
+        }
+        catch (IOException x) {
+            System.err.println("Could not read input. Something happend");
+        }
+
+        System.out.println(input);
+
+    }
 }
