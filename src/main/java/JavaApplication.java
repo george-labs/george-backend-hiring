5a0e2a8b63b575b46d7c5010a41bff8d317f8795
Tobias Mueller <tobias.mu@protonmail.com>
2023-09-25 10:03:12 +0100

    Session_2 end

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,8 +1,16 @@
+import sentence.Sentence;
+import sentence.Word;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import static java.lang.System.exit;
+import static java.lang.System.setOut;
+
 public class JavaApplication {
+    private static int TEXT_INVALI_EXIT = 5;
+
 
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
@@ -11,15 +19,17 @@ public class JavaApplication {
         // Enter data using BufferReader
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
-        String input = null;
+
+        var input = "";
         try{
             input = reader.readLine();
         }
         catch (IOException x) {
-            System.err.println("Could not read input. Something happend");
+            System.err.println("Could not read input. Something happened");
+            exit(TEXT_INVALI_EXIT);
         }
+        var sentence = new Sentence(input);
 
-        System.out.println(input);
-
+        System.out.println("Number of words: " + sentence.getWords().size());
     }
 }
