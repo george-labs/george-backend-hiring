4d01c962c8de02193535c17c1bcf6720e5ad60d4
Henry Odohi Unah <henry.unah@gmail.com>
2023-10-20 14:37:07 +0200

    updated code assumptions and refactored the main method to accept program arguments without having exceptions

--- docs/assumptions.md
+++ docs/assumptions.md
@@ -1,3 +1,7 @@
 # Assumptions per iteration
 
-## Iteration 1
+## Iteration 3
+
+The user can execute the application by passing the filename as an argument
+
+If no argument is passed the application expects the user to enter the correct text
--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -3,18 +3,17 @@ import java.util.Scanner;
 public class JavaApplication {
 
     public static void main(String[] args){
-        long output;
-        String fileName = "mytext.txt";
+        long numberOfWords;
+        String fileName = (args.length != 0) ? args[0] : "";
         Scanner scanner = new Scanner(System.in);
         WordCounter wordCounter = new WordCounter();
         if(fileName.isEmpty()){
-            String input = scanner.nextLine();
-            output = wordCounter.countWordsFromText(input);
-            System.out.println("Number of words: " + output);
-        }else{
             System.out.print("Enter text: ");
-            output = wordCounter.countWordsFromFile(fileName);
-            System.out.println("Number of words: " + output);
+            String input = scanner.nextLine();
+            numberOfWords = wordCounter.countWordsFromText(input);
+        }else{
+            numberOfWords = wordCounter.countWordsFromFile(fileName);
         }
+        System.out.println("Number of words: " + numberOfWords);
     }
 }
