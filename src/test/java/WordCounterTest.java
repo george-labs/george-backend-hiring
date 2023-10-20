2290ee678946378e9910394e573f10fce02eb0c9
Henry Odohi Unah <henry.unah@gmail.com>
2023-10-20 15:02:07 +0200

    added unit test for counting words that are read from file and corrected assertion order from expected to actual

--- src/test/java/WordCounterTest.java
+++ src/test/java/WordCounterTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WordCounterTest {
@@ -11,31 +15,47 @@ public class WordCounterTest {
     @Test
     public void wordCount(){
         String text = "Mary had two little lambs";
-        long result = wordCounter.countWords(text);
-        assertEquals(result, 5);
+        long result = wordCounter.countWordsFromText(text);
+        assertEquals(5, result);
     }
 
     @DisplayName("Given a text with numbers or wrong characters the words should not be counted")
     @Test
     public void wordCountWithNumbers(){
         String text = "Mary2 had a little l7";
-        long result = wordCounter.countWords(text);
-        assertEquals(result, 2);
+        long result = wordCounter.countWordsFromText(text);
+        assertEquals(2, result);
     }
 
     @DisplayName("Given a text with special characters the words should not be counted")
     @Test
     public void wordCountWithSpecialCharacter(){
         String text = "Mary2 had, a little l7";
-        long result = wordCounter.countWords(text);
-        assertEquals(result, 1);
+        long result = wordCounter.countWordsFromText(text);
+        assertEquals(1, result);
     }
 
     @DisplayName("Given a text with a stop word then the stop word should not be counted ")
     @Test
     public void wordCountWithStopWords(){
         String text = "Mary had a little lamb";
-        long result = wordCounter.countWords(text);
-        assertEquals(result, 4);
+        long result = wordCounter.countWordsFromText(text);
+        assertEquals(4, result);
+    }
+
+    @DisplayName("Given an input file then the words in the file are counted")
+    @Test
+    public void wordCountFromFileInput(){
+        try {
+            File tmpfile = File.createTempFile("testFile", ".txt");
+            tmpfile.deleteOnExit();
+            FileWriter fw = new FileWriter(tmpfile);
+            fw.write("Mary has a little lamb");
+            fw.close();
+            long numberOfWords = wordCounter.countWordsFromFile(tmpfile.getPath());
+            assertEquals(4, numberOfWords);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
