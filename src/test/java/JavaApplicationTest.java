4e92daf752bbd16bfca89000ca71dbb566a552aa
Claus Polanka <claus.polanka@erstegroup.com>
2023-11-09 17:04:39 +0100

    Fix running Java tests

--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,2 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class JavaApplicationTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(true, true);
+    }
 }
