35977c0e623fad1fd6404eab9a31f5e12d13e497
Claus Polanka <claus.polanka@erstegroup.com>
2023-11-09 17:21:08 +0100

    Undo dummy test

--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,9 +1,2 @@
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-
 public class JavaApplicationTest {
-    @Test
-    public void dummyTest() {
-        assertEquals(true, true);
-    }
 }
