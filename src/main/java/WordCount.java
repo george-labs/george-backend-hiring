2ddf321c3c37a86d59836376005b8b0d8e7614f7
Yalda Ghasemi <yaldapersian84@gmail.com>
2023-11-17 13:31:38 +0330

    write  word  count with word stopper file

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,7 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.Set;
+
 
 public class WordCount {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+
+
         Scanner  scanner = new Scanner(System.in);
         System.out.println("Please enter  text:");
         String  text  = scanner.nextLine();
@@ -11,18 +20,40 @@ public class WordCount {
 
 
     }
-    public static int wordCount(String text){
+    private static String readStopFile() throws IOException {
+        ClassLoader classLoader = WordCount.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("stopwords.txt");
+        String data = readFromInputStream(inputStream);
+        return data;
+    }
+    private static String readFromInputStream(InputStream inputStream)
+            throws IOException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br
+                     = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
+        }
+        return resultStringBuilder.toString();
+    }
+
+    public static int wordCount(String text ) throws IOException {
         if  (text == null || text.isEmpty())
                 return  0;
 
-
-
         String[] splited = text.split("\\s+");
+        Set<String> stopWords = Set.of(readStopFile().split("\\n"));
+
         int counter =0;
 
-        for( String  s: splited) {
-            if (s.matches("^[a-zA-Z]*$"))
-                counter++;
+        for( String  word: splited) {
+
+
+            if (word.matches("^[a-zA-Z]*$"))
+                if(!stopWords.contains(word))
+                    counter++;
         }
 
         return counter;
--- src/test/java/WordCountTest.java
+++ src/test/java/WordCountTest.java
@@ -1,10 +1,12 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 public class WordCountTest {
 
     @Test
-    public void testWithNumbers() {
+    public void testWithNumbers() throws IOException {
 
        int actual = WordCount.wordCount("I have 2  apples");
 
@@ -12,33 +14,49 @@ public class WordCountTest {
     }
 
     @Test
-    public void testWithMix() {
+    public void testWithMix() throws IOException {
 
         int actual = WordCount.wordCount("I have222 two apples");
 
         Assertions.assertEquals(3, actual);
     }
     @Test
-    public void testWithLetters() {
+    public void testWithLetters() throws IOException {
 
         int actual = WordCount.wordCount("I have UU apples");
 
         Assertions.assertEquals(4, actual);
     }
     @Test
-    public void testWithSigns() {
+    public void testWithSigns() throws IOException {
 
         int actual = WordCount.wordCount("I have &  *");
 
         Assertions.assertEquals(2, actual);
     }
     @Test
-    public void testWithEmpty() {
+    public void testWithEmpty() throws IOException {
 
         int actual = WordCount.wordCount(" ");
 
         Assertions.assertEquals(0, actual);
     }
+
+    @Test
+    public void testWithOneStopWords() throws IOException {
+
+        int actual = WordCount.wordCount("The  sun is beautiful");
+
+        Assertions.assertEquals(4, actual);
+    }
+    @Test
+    public void testWithOneStopWords2() throws IOException {
+
+        int actual = WordCount.wordCount("the  sun is beautiful");
+
+        Assertions.assertEquals(3, actual);
+    }
+
 
 
 
