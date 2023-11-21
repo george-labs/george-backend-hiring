53faf6bfabf432d542cb3e0cec05b7e40275a151
Alexander Steindl <alexander.steindl@me.com>
2023-11-21 10:49:18 +0100

    test CLI part of „Word Count I“

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,18 +1,25 @@
+import java.io.*;
 import java.util.Scanner;
 
 public class JavaApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        countWords(System.in, System.out);
+    }
+
+    public static void countWords(InputStream inputStream, OutputStream outputStream) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+        writer.write("Enter text: ");
+        writer.flush();
+
         WordCounter wordCounter = new WordCounter();
-
-        System.out.print("Enter text: ");
-
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(inputStream);
 
         if (sc.hasNextLine()) {
             String line = sc.nextLine();
             long wordCount = wordCounter.countWords(line);
 
-            System.out.printf("Number of words: %d%n", wordCount);
+            writer.write(String.format("Number of words: %d", wordCount));
+            writer.flush();
         }
     }
 }
--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,9 +1,20 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class JavaApplicationTest {
+import java.io.*;
+
+class JavaApplicationTest {
     @Test
-    public void dummyTest() {
-        Assertions.assertTrue(true);
+    void testCountWords() throws IOException {
+        String input = "Mary had a little lamb";
+        String expectedOutput = "Enter text: Number of words: 5";
+
+        try (InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+             ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            JavaApplication.countWords(inputStream, outputStream);
+            String output = outputStream.toString();
+
+            Assertions.assertEquals(expectedOutput, output);
+        }
     }
 }
