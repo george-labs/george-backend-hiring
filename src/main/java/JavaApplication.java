bebe38a35adc05d981d293a94b40aca310bfeffa
Alexander Steindl <alexander.steindl@me.com>
2023-11-21 10:31:33 +0100

    implemented CLI part of iteration 1 (Word Count I)

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class JavaApplication {
+    public static void main(String[] args) {
+        WordCounter wordCounter = new WordCounter();
+
+        System.out.print("Enter text: ");
+
+        Scanner sc = new Scanner(System.in);
+
+        if (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            long wordCount = wordCounter.countWords(line);
+
+            System.out.printf("Number of words: %d%n", wordCount);
+        }
+    }
 }
