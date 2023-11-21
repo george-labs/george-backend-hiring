b469c29ccd9046dfebf3add4e8f1918e73d835be
Alexander Steindl <alexander.steindl@me.com>
2023-11-21 13:02:28 +0100

    submit current status

--- src/main/java/WordCounts.java
+++ src/main/java/WordCounts.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class WordCounts {
+public class WordCounts {
+    private final long wordCount;
+    private final long uniqueWordCount;
+
+    public WordCounts(long wordCount, long uniqueWordCount) {
+        this.wordCount = wordCount;
+        this.uniqueWordCount = uniqueWordCount;
+    }
+
+    public long getWordCount() {
+        return wordCount;
+    }
+
+    public long getUniqueWordCount() {
+        return uniqueWordCount;
+    }
 }
