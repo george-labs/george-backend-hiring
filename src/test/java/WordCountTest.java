d1b1edce9c81c4e23168295d2e20e7efc32088c5
Salimify <salim.bnhassine@gmail.com>
2023-12-10 16:05:20 +0100

    adding a dummy method and a dummy test

---
+++ src/test/java/WordCountTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WorldCountTest {
+
+    @Test
+    public void test() {
+        int x = 1 + 2;
+        assertEquals(3, x);
+    }
+}
