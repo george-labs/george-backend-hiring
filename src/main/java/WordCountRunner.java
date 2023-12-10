8cab9e85b4fbbcc1cf0894fd8cfe2f6fd7c9ccf4
Salimify <salim.bnhassine@gmail.com>
2023-12-10 21:46:34 +0100

    Optimizing implementation

--- src/main/java/WordCountRunner.java
+++ src/main/java/WordCountRunner.java
@@ -1,25 +1,32 @@
 import java.util.Scanner;
 
 public class WordCountRunner {
-    public static void main(String[] args) {
-        WordCount wordCount = WordCount.getInstance();
-        if (args.length > 0) {
-            String filename = args[0];
-            if (WordFileReader.isValidFileName(filename)) {
-                System.out.println("Number of words: " + wordCount.countText(filename, true));
-            } else {
-                System.out.println("Invalid filename argument provided");
-            }
-        } else {
-            try {
-                System.out.print("Enter text: ");
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Number of words: " + wordCount.countText(scanner.nextLine()));
-                scanner.close();
-            } catch (IllegalArgumentException e) {
-                System.out.println("Error: " + e.getMessage());
-            }
-        }
+    private static final WordCount wordCount = WordCount.getInstance();
 
+    private static void handleFileNameInput(String fileName) {
+        if (WordFileReader.isValidFileName(fileName)) {
+            System.out.println("Number of words: " + wordCount.countText(fileName, true));
+        } else {
+            System.out.println("Invalid filename argument provided");
+        }
+    }
+
+    public static void handleTextInput() {
+        try {
+            System.out.print("Enter text: ");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Number of words: " + wordCount.countText(scanner.nextLine()));
+            scanner.close();
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            handleFileNameInput(args[0]);
+        } else {
+            handleTextInput();
+        }
     }
 }
