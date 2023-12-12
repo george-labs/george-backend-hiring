99cdd53367b551849f3f381c65dc96db48028ccb
Timotej Kubas <kubas.timo@yahoo.com>
2023-12-12 10:33:00 +0100

    WIP Implemented simple WordCounter

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -1,22 +1,24 @@
-import java.util.regex.Matcher;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class WordCounter {
 
-    private static final String REGEX = "[a-zA-Z]";
+    private static final String REGEX = "[a-zA-Z]+";
     private static final String WORD_DELIMITER = " ";
     private static Pattern pattern = Pattern.compile(REGEX);
 
-    public int count(String sentence) {
+    public long count(String sentence) {
         String[] words = splitSentence(sentence);
+        return countWords(words);
+    }
 
-        Matcher matcher = pattern.matcher(sentence);
-
+    private static long countWords(String[] words) {
+        return Arrays.stream(words)
+                .filter(word -> pattern.matcher(word).matches())
+                .count();
     }
 
     private String[] splitSentence(String sentence) {
         return sentence.split(WORD_DELIMITER);
     }
-
-
 }
---
+++ src/test/java/WordCounterTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class WordCounterTest {
+
+    @Test
+    public void GIVEN_sentence_WHEN_count_THEN_wordsCounted() {
+        //GIVEN
+        WordCounter wordCounter = new WordCounter();
+        String sentence = "Hello World";
+        long expectedCount = 2;
+
+        //WHEN
+        long actualCount = wordCounter.count(sentence);
+
+        //THEN
+        Assertions.assertEquals(expectedCount, actualCount);
+    }
+
+    @Test
+    public void GIVEN_wordsWithSpecialCharacters_WHEN_count_THEN_countedOnlyRegularWords() {
+        //GIVEN
+        WordCounter wordCounter = new WordCounter();
+        String sentence = "'Hello W0rld";
+        long expectedCount = 0;
+
+        //WHEN
+        long actualCount = wordCounter.count(sentence);
+
+        //THEN
+        Assertions.assertEquals(expectedCount, actualCount);
+    }
+
+}
