c7f915f1e96a93c3d31f9ba7157aa5491648f3d2
Timotej Kubas <kubas.timo@yahoo.com>
2023-12-12 11:00:56 +0100

    WIP Added test for the main method

--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,20 +1,32 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 public class JavaApplicationTest {
 
     @Test
     public void GIVEN_words_WHEN_countTheWords_THEN_wordsCounted() {
-
         //GIVEN
         JavaApplication javaApplication = new JavaApplication();
         String wordToCount = "Hello world";
+        final InputStream original = System.in;
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(wordToCount.getBytes());
+        System.setIn(inputStream);
+        final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+        String wordsCount = outputStreamCaptor.toString();
+        String expectedWordCount = "2";
 
         //WHEN
+        JavaApplication.main(null);
 
 
         //THEN
-        Assertions.assertTrue(true);
+        Assertions.assertEquals(expectedWordCount, wordsCount);
     }
 
 }
