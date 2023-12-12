9d8cdb6df62fb4e8cea0c675d7a26549cebd61ac
Timotej Kubas <kubas.timo@yahoo.com>
2023-12-12 10:40:56 +0100

    WIP Added main method

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class JavaApplication {
+
+    public static void main(String args[]) {
+        WordCounter wordCounter = new WordCounter();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the text: ");
+
+        long wordsCount = wordCounter.count(input.nextLine());
+        System.out.println("Number of words: " + wordsCount);
+    }
+
 }
