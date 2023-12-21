c1e5b0118091f0039741d857504121de74017efa
Volodymyr Salo <volodymyr.salo@gmail.com>
2023-12-21 10:50:34 +0100

    Iteration1 - Work correctly with empty string

--- src/main/kotlin/KotlinApplication.kt
+++ src/main/kotlin/KotlinApplication.kt
@@ -1,5 +1,5 @@
 fun countWords(text: String): Int {
-    return if (text.isNotEmpty()) text.split(" ").filter { word -> word.all { it.isLetter() } }.size else 0
+    return text.split(" ").filter { word -> word.isNotEmpty() }.filter { word -> word.all { it.isLetter() } }.size
 }
 
 fun main(args: Array<String>) {
--- src/test/kotlin/KotlinApplicationTest.kt
+++ src/test/kotlin/KotlinApplicationTest.kt
@@ -4,25 +4,33 @@ import org.junit.jupiter.api.Test
 class KotlinApplicationTest {
     @Test
     fun `test word count`() {
-        val numberOdWords = countWords("Hello world")
-        Assertions.assertEquals(2,  numberOdWords)
+        val numberOfWords = countWords("Hello world")
+        Assertions.assertEquals(2,  numberOfWords)
     }
 
     @Test
     fun `test incorrect words are skipped`() {
-        val numberOdWords = countWords("Hell0 wor1d")
-        Assertions.assertEquals(0,  numberOdWords)
+        val numberOfWords = countWords("Hell0 wor1d")
+        Assertions.assertEquals(0,  numberOfWords)
     }
 
     @Test
     fun `test that we skip also words with commas, semicolons, etc`() {
-        val numberOdWords = countWords("Hello, world! We: should; skip| this.")
-        Assertions.assertEquals(0,  numberOdWords)
+        val numberOfWords = countWords("Hello, world! We: should; skip| this.")
+        Assertions.assertEquals(0,  numberOfWords)
     }
 
     @Test
     fun `test empty string`() {
-        val numberOdWords = countWords("")
-        Assertions.assertEquals(0,  numberOdWords)
+        val numberOfWords = countWords("")
+        Assertions.assertEquals(0,  numberOfWords)
+    }
+
+    @Test
+    fun `test that words containing numbers or separators with space after at the end of the string are not counted`() {
+        var numberOfWords = countWords("qwwq4 ")
+        Assertions.assertEquals(0,  numberOfWords)
+        numberOfWords = countWords("qwwqwerrw,  ")
+        Assertions.assertEquals(0,  numberOfWords)
     }
 }
