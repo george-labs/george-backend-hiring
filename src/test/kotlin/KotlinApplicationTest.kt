89cbe53606f58aa8abbd9623e383ae7bbc93f995
Volodymyr Salo <volodymyr.salo@gmail.com>
2023-12-21 10:24:04 +0100

    Iteration1 - Write more unit tests to cover edge cases

--- src/test/kotlin/KotlinApplicationTest.kt
+++ src/test/kotlin/KotlinApplicationTest.kt
@@ -7,4 +7,16 @@ class KotlinApplicationTest {
         val numberOdWords = countWords("Hello world")
         Assertions.assertEquals(2,  numberOdWords)
     }
+
+    @Test
+    fun `test incorrect words are skipped`() {
+        val numberOdWords = countWords("Hell0 wor1d")
+        Assertions.assertEquals(0,  numberOdWords)
+    }
+
+    @Test
+    fun `test that we skip also words with commas, semicolons, etc`() {
+        val numberOdWords = countWords("Hello, world! We: should; skip| this.")
+        Assertions.assertEquals(0,  numberOdWords)
+    }
 }
