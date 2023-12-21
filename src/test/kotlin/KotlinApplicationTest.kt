96436941ebc2dfca9ed1043639b45302227723da
Volodymyr Salo <volodymyr.salo@gmail.com>
2023-12-21 13:38:03 +0100

    Iteration2 - Reformat test cases a bit

--- src/test/kotlin/KotlinApplicationTest.kt
+++ src/test/kotlin/KotlinApplicationTest.kt
@@ -7,38 +7,46 @@ class KotlinApplicationTest {
     @Test
     fun `test word count`() {
         val numberOfWords = countWords("Hello world")
+
         Assertions.assertEquals(2,  numberOfWords)
     }
 
     @Test
     fun `test incorrect words are skipped`() {
         val numberOfWords = countWords("Hell0 wor1d")
+
         Assertions.assertEquals(0,  numberOfWords)
     }
 
     @Test
     fun `test that we skip also words with commas, semicolons, etc`() {
         val numberOfWords = countWords("Hello, world! We: should; skip| this.")
+
         Assertions.assertEquals(0,  numberOfWords)
     }
 
     @Test
     fun `test empty string`() {
         val numberOfWords = countWords("")
+
         Assertions.assertEquals(0,  numberOfWords)
     }
 
     @Test
     fun `test that words containing numbers or separators with space after at the end of the string are not counted`() {
         var numberOfWords = countWords("qwwq4 ")
+
         Assertions.assertEquals(0,  numberOfWords)
+
         numberOfWords = countWords("qwwqwerrw,  ")
+
         Assertions.assertEquals(0,  numberOfWords)
     }
 
     @Test
     fun `test stop words are taken into account`() {
         val stopWords = setOf("a", "the", "an")
+
         val numberOfWords = countWords("A quick brown fox jumps over the lazy dog", stopWords)
 
         Assertions.assertEquals(8,  numberOfWords)
@@ -47,16 +55,21 @@ class KotlinApplicationTest {
     @Test
     fun `test file does not exist`() {
         val dummyFileName = "nonexistentfile"
+
         val doesExist = checkStopWordsFileExists(dummyFileName)
+
         Assertions.assertFalse(doesExist)
     }
 
     @Test
     fun `test file exists`() {
         val dummyFileName = "somefile.txt"
+
         File(dummyFileName).createNewFile()
         val doesExist = checkStopWordsFileExists(dummyFileName)
+
         Assertions.assertTrue(doesExist)
+
         File(dummyFileName).delete()
     }
 
