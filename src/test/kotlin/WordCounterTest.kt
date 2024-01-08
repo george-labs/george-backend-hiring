9a6cd2f81ed1e93406faa54be73af21dda03d296
Jaroslav Zigo <jarozigo33@gmail.com>
2024-01-08 11:12:58 +0100

    feat: add test

--- src/test/kotlin/WordCounterTest.kt
+++ src/test/kotlin/WordCounterTest.kt
@@ -1,40 +1,44 @@
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class WordCounterTest {
 
+    private lateinit var wordCounter: WordCounter
+
+    @BeforeEach
+    fun setUp() {
+        wordCounter = WordCounter()
+    }
+
     @Test
     fun testWordsWithAlphaCharactersOnly() {
-        val wordCounter = WordCounter()
-
         assertEquals(3, wordCounter.count("word word word"))
     }
 
     @Test
     fun testWordsWithAlphaCharactersAndSpacesBeforeAndAfter() {
-        val wordCounter = WordCounter()
-
         assertEquals(3, wordCounter.count("     word word word     "))
     }
 
     @Test
     fun testWordsWithNonAlphaCharacters() {
-        val wordCounter = WordCounter()
-
         assertEquals(2, wordCounter.count("word w1o2r3d word word,"))
     }
 
     @Test
     fun testWordsContainingOnlyNonAlphaCharacters() {
-        val wordCounter = WordCounter()
-
         assertEquals(0, wordCounter.count("word1, word. *&!"))
     }
 
     @Test
     fun testEmptyWords() {
-        val wordCounter = WordCounter()
-
         assertEquals(0, wordCounter.count(""))
+    }
+
+    @Test
+    fun testWordsFromGivenFileAreSuccessfullyIgnored() {
+        val wordsToIgnore = listOf("the", "abc")
+        assertEquals(2, wordCounter.count("word the word abc", wordsToIgnore = wordsToIgnore))
     }
 }
