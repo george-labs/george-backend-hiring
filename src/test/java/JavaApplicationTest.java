66626dbf295cccf499eb8595f50543e0f2614a81
Viacheslav Demianenko <urbanderwil@gmail.com>
2024-01-09 15:10:27 +0200

    Changes in tests

--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.io.PrintStream;
 
 public class JavaApplicationTest {
