0af421f4b54715c54c86c7a19787a4a2580be0aa
Šimon Kocúrek <simon.kocurek@gmail.com>
2024-01-18 09:27:04 +0100

    Finish iteration 1

--- docs/assumptions.md
+++ docs/assumptions.md
@@ -1,3 +1,5 @@
 # Assumptions per iteration
 
 ## Iteration 1
+
+- Words containing numbers or special characters it counts as 2 words (`te12st` counts as 2 words `te` and `st`).
--- src/main/kotlin/KotlinApplication.kt
+++ src/main/kotlin/KotlinApplication.kt
@@ -1,2 +1,12 @@
-class KotlinApplication {
+
+fun main(args: Array<String>) {
+    print("Enter text: ")
+    val inputText = readln()
+
+    val wordCount = countWords(inputText)
+    println("Number of words: $wordCount")
+}
+
+fun countWords(text: String): Int {
+    return "([a-zA-Z]+)".toRegex().findAll(text).count()
 }
