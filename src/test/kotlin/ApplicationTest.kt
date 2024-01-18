8dc1dd6a80063bc7368ed5d85d248368b45bb25a
Šimon Kocúrek <simon.kocurek@gmail.com>
2024-01-18 10:25:19 +0100

    Add integration test for reading input from argument file

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -1,23 +1,41 @@
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import java.io.ByteArrayOutputStream
 import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.io.path.absolutePathString
 import kotlin.io.path.deleteIfExists
 import kotlin.io.path.writeText
 
 class ApplicationTest {
 
+    companion object {
+        private lateinit var testStopWordsFile: Path
+
+        @JvmStatic
+        @BeforeAll
+        fun setup() {
+            testStopWordsFile = Files.createTempFile("testStopWords", "txt")
+            testStopWordsFile.writeText("""
+                the
+                a
+                on
+                off
+            """.trimIndent())
+        }
+
+        @JvmStatic
+        @AfterAll
+        fun cleanup() {
+            testStopWordsFile.deleteIfExists()
+        }
+    }
+
     @Test
     fun countWordsInSentence() {
         // Given
-        val testStopWordsFile = Files.createTempFile("testStopWords", "txt")
-        testStopWordsFile.writeText("""
-            the
-            a
-            on
-            off
-        """.trimIndent())
-
         val outputStream = ByteArrayOutputStream(1024)
 
         val counter = Application(
@@ -32,7 +50,33 @@ class ApplicationTest {
 
         // Then
         Assertions.assertEquals("Enter text: Number of words: 4\n", outputStream.toString())
-        testStopWordsFile.deleteIfExists()
+    }
+
+    @Test
+    fun countWordsFromProvidedFile() {
+        // Given
+        val testInputFile = Files.createTempFile("testInputFile", "txt")
+        testInputFile.writeText("""
+            Mary had
+            a little
+            lamb
+        """.trimIndent())
+
+        val outputStream = ByteArrayOutputStream(1024)
+
+        val counter = Application(
+            arguments = arrayOf(testInputFile.absolutePathString()),
+            stopWordsFile = testStopWordsFile.toFile(),
+            textInputStream = "Mary had a little lamb".byteInputStream(),
+            resultOutputStream = outputStream,
+        )
+
+        // When
+        counter.run()
+
+        // Then
+        Assertions.assertEquals("Number of words: 4\n", outputStream.toString())
+        testInputFile.deleteIfExists()
     }
 
 }
