46cc34c972fc98c913e562e1c310570abbe55c5a
Doni41 <hegyesia0607@yahoo.com>
2024-02-12 15:03:06 +0100

    Removing validator from unit tests

--- src/test/java/unit/ItemProcessorTest.java
+++ src/test/java/unit/ItemProcessorTest.java
@@ -6,21 +6,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import processor.ItemProcessor;
 import processor.Processor;
-import validator.ConsoleInputValidator;
-import validator.Validator;
 
 import java.util.List;
 
 public class ItemProcessorTest {
     
     private static final String REGEX_ONLY_ALPHABET = "^[a-zA-Z]+$";
-    private Validator validator;
     private Processor processor;
 
     @BeforeEach
     public void initValidator() {
-        this.validator = new ConsoleInputValidator(REGEX_ONLY_ALPHABET);
-        this.processor = new ItemProcessor(this.validator);
+        this.processor = new ItemProcessor();
     }
 
     @Test
