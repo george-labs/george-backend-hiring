51774d2c6ca92543c2561935a8fdc5942f99f067
marekmaruna <37212014+marekmaruna@users.noreply.github.com>
2024-03-01 10:00:37 +0100

    Unfinished counter of words.

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,29 @@
+import java.io.Console;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class JavaApplication {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter text: ");
+        String text = scanner.nextLine();
+
+        var words = text.split("[\\s]");
+        Pattern pattern = Pattern.compile("[a-zA-Z]");
+
+
+        long counter = 0;
+        for(String word: words){
+            Matcher matcher = pattern.matcher(word);
+            if(matcher.matches()){
+                counter ++;
+            }
+        }
+
+        System.out.println(counter);
+
+        //
+    }
 }
--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,2 +1,8 @@
+import org.junit.jupiter.api.Test;
+
 public class JavaApplicationTest {
+    @Test
+    void testShowSomething() {
+        System.out.println("Dummy test");
+    }
 }
