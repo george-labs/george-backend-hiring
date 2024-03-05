f5a376cb16e06a5987e456e961b78ec58872d142
Juraj Olos <olos.juraj@gmail.com>
2024-03-05 14:21:18 +0100

    refactoring - split wordStats service logic

--- src/main/java/bl/services/WordStatsServiceImpl.java
+++ src/main/java/bl/services/WordStatsServiceImpl.java
@@ -20,24 +20,33 @@ public class WordStatsServiceImpl implements WordStatsService {
             return new WordStats(0, 0, 0.0);
         }
 
-        Set<String> stopWords = stopWordsProvider.getStopWords();
+        List<String> words = extractWords(input);
+        Set<String> uniqueWords = new HashSet<>(words);
+        double avg = calcAvg(words);
 
+        return new WordStats(words.size(), uniqueWords.size(), avg, createIndex(uniqueWords, createIndex));
+    }
+
+    private List<String> extractWords(String input) {
+        Set<String> stopWords = stopWordsProvider.getStopWords();
         String[] wordCandidates = input.split("\\s+");
 
-        List<String> words = Arrays.stream(wordCandidates)
+        return Arrays.stream(wordCandidates)
                 .map(candidate -> candidate.replaceAll("[.,?!]$", ""))
                 .filter(candidate -> candidate.matches("[a-zA-Z\\-]+"))
                 .filter(candidate -> !stopWords.contains(candidate))
                 .collect(Collectors.toList());
-        Set<String> uniqueWords = new HashSet<>(words);
+    }
 
+    private double calcAvg(List<String> words) {
+        if (words.isEmpty()) {
+            return 0.0;
+        }
         double totalLength = 0;
         for (String word : words) {
             totalLength += word.length();
         }
-        double avg = words.isEmpty() ? 0.0 : totalLength / words.size();
-
-        return new WordStats(words.size(), uniqueWords.size(), avg, createIndex(uniqueWords, createIndex));
+        return totalLength / words.size();
     }
 
     private List<String> createIndex(Set<String> words, boolean createIndex) {
