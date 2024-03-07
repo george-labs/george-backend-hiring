10fb4d9445fca7b0a474db6f5d18bc57d1ad748b
vkoci <koci.vladimir@gmail.com>
2024-03-07 12:00:41 +0100

    wip stopwords filter

--- docs/assumptions.md
+++ docs/assumptions.md
@@ -6,3 +6,10 @@
 - User input won't contain newline character(s)
 - Words are always separated by whitespaces
 - Everything except [a-zA-Z] is an invalid word
+
+
+## Iteration 2
+
+- stopwords.txt on classpath; we won't provide a way for user to change this currently
+- stopwords file will contain 1 word per line
+- stopwords file won't be user-supplied - all word will be valid, no need to check
--- src/main/java/wordcount/CharacterWordCounter.java
+++ src/main/java/wordcount/CharacterWordCounter.java
@@ -1,23 +1,39 @@
 package wordcount;
 
 import java.util.Arrays;
+import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class CharacterWordCounter implements WordCounter {
 
 	private Pattern characterRegexPattern;
+	private Set<String> stopWords;
 
 	CharacterWordCounter() {
 		characterRegexPattern = Pattern.compile("[a-zA-Z]+");
 	}
 
+	/**
+	 * Returns true when word is in the stopwords collection
+	 * @param word
+	 * @return
+	 */
+	private boolean filterStopWords(String word) {
+		return stopWords.contains(word);
+	}
+	
+	public void supplyStopWords(Set<String> stopWords) {
+		this.stopWords = stopWords;
+	}
+	
 	@Override
 	public long countWords(String input) {
 		String[] wordCandidates = input.split("\\s");
 		Stream<String> wordCandidatesStream = Arrays.stream(wordCandidates);
 		wordCandidatesStream = wordCandidatesStream
-				.filter(candidate -> characterRegexPattern.matcher(candidate).matches());
+				.filter(candidate -> characterRegexPattern.matcher(candidate).matches())
+				.filter(candidate -> filterStopWords(candidate));
 
 		return wordCandidatesStream.count();
 	}
