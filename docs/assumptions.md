df85853273d13113b02174948391a26e5bd0f3b5
vkoci <koci.vladimir@gmail.com>
2024-03-07 09:58:02 +0100

    wip prototype

--- docs/assumptions.md
+++ docs/assumptions.md
@@ -1,3 +1,8 @@
 # Assumptions per iteration
 
 ## Iteration 1
+
+- User won't enter malicious code (can't use proper user input sanitizing - requires an external library); just do basic sanitizing
+- Words are always separated by whitespaces
+- Everything except [a-zA-Z] is an invalid word
+- User input won't contain newline character(s)
--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class JavaApplication {
+	public static void main(String[] args) {
+		// first rough prototype implementation, just for the integration tests
+		
+		// ui
+		// 1. prompt
+		System.out.print("Enter text: ");
+		// 2. read input
+		String line;
+		try (Scanner scanner = new Scanner(System.in)) {
+			line = scanner.nextLine();
+		}
+		
+		// bl
+		// 3. count words
+		String[] wordCandidates = line.split("/s");
+		Arrays.stream(wordCandidates);
+		
+		// ui
+		// 4. present output
+	}
 }
