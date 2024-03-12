393c5301c8f117d2988f100b137b9b33fb15a875
sotiabhishek <123250238+sotiabhishek@users.noreply.github.com>
2024-03-12 14:32:05 +0530

    Work in progress.

--- docs/assumptions.md
+++ docs/assumptions.md
@@ -1,3 +1,9 @@
 # Assumptions per iteration
 
 ## Iteration 1
+
+Split
+
+valid word
+
+count
--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
 public class JavaApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter text: ");
+        String inputLine = scan.nextLine();
+        //counting logic
+        System.out.println("Number of words: " + countWords(inputLine));
+    }
+
+    public static int countWords(String inputLine) {
+
+        int answer = 0;
+
+        String[] splittedInput = inputLine.split(" ");
+
+        for(String possibleWord : splittedInput){
+            boolean isValidWord = isValidWord(possibleWord);
+            if(isValidWord){
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static boolean isValidWord(String possibleWord){
+        return possibleWord.matches("[a-zA-Z]+");
+    }
+
 }
