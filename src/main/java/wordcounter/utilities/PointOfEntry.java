56a4a8643bb464e60e9122c145e4e03d3854876f
sotiabhishek <123250238+sotiabhishek@users.noreply.github.com>
2024-03-12 19:41:09 +0530

    Added implementation for scenario3 for reading the input file from user provided filename. Due to lack to time, unable to add the test cases for it now.

--- src/main/java/wordcounter/utilities/PointOfEntry.java
+++ src/main/java/wordcounter/utilities/PointOfEntry.java
@@ -1,13 +1,20 @@
 package wordcounter.utilities;
 
+import wordcounter.functionalities.FileIOLogicImpl;
 import wordcounter.functionalities.InputOutputLogic;
 import wordcounter.functionalities.impl.ConsoleIOLogicImpl;
+
+import java.util.Objects;
 
 public class PointOfEntry {
     private final InputOutputLogic inputOutputLogic;
 
-    public PointOfEntry(){
-        this.inputOutputLogic = new ConsoleIOLogicImpl();
+    public PointOfEntry(String[] args){
+        if(Objects.nonNull(args) && !args[0].isEmpty()){
+            this.inputOutputLogic = new FileIOLogicImpl(args[0]);
+        }else{
+            this.inputOutputLogic = new ConsoleIOLogicImpl();
+        }
         runIOLogic();
     }
 
