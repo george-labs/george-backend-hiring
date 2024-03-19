b733f3786a83e8f7b434026f8e330acfe0d5161b
Xenofon Zinoviou <xzinoviou@gmail.com>
2024-03-19 15:32:41 +0200

    java app run iteration 1

--- src/main/java/interview/JavaApplication.java
+++ src/main/java/interview/JavaApplication.java
@@ -1,4 +1,21 @@
 package interview;
 
+import interview.business.WordCounter;
+import interview.output.WordCountOutput;
+import interview.presenter.WordCountPresenter;
+import interview.reader.ConsoleInputReader;
+import interview.reader.InputReader;
+
 public class JavaApplication {
+
+    public static void main(String[] args) {
+
+        InputReader inputReader = new ConsoleInputReader(System.in);
+        WordCounter wordCounter = new WordCounter(inputReader);
+        final WordCountOutput output = wordCounter.processInput();
+
+        WordCountPresenter presenter = new WordCountPresenter();
+
+        presenter.presentWordCount(output);
+    }
 }
