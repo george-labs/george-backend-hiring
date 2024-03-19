0e55771cc083eadd00f333839e16177a86ad9b94
Xenofon Zinoviou <xzinoviou@gmail.com>
2024-03-19 11:39:28 +0200

    add reader exception for input reader failing cases

---
+++ src/main/java/interview/exception/ReaderException.java
@@ -0,0 +1,11 @@
+package interview.exception;
+
+/**
+ * @author : Xenofon Zinoviou
+ */
+public class ReaderException extends RuntimeException {
+
+    public ReaderException(String message) {
+        super(message);
+    }
+}
