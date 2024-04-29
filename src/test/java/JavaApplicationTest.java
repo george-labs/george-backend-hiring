250122ee9d603c3e94a1cfa5671b5a582a571ca7
Juraj Benko <juraj.benko@gmail.com>
2024-04-29 10:25:33 +0200

    session2, iteration1 finished

--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,10 +1,29 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 public class JavaApplicationTest {
 
     @Test
-    public void test_it1_mary() {
+    public void testMary() {
         String testedString = "Mary had a little lamb";
-        //countWord
+        int wordCount = JavaApplication.countWords(testedString);
+        assertEquals(wordCount, 5);
     }
+
+    @Test
+    public void testWithSpecialCharacters() {
+        String testedString = "Mary . \n word2jdjdj 333";
+        int wordCount = JavaApplication.countWords(testedString);
+        assertEquals(wordCount, 1);
+    }
+
+    @Test
+    public void testEmptyString() {
+        String testedString = "";
+        int wordCount = JavaApplication.countWords(testedString);
+        assertEquals(wordCount, 0);
+    }
+
 }
