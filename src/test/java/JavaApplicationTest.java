5c2612aa8d6520b394be4d92c3409bc9dd5f9c2c
Juraj Benko <juraj.benko@gmail.com>
2024-04-29 10:26:00 +0200

    session2, iteration1 finished

--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class JavaApplicationTest {
 
