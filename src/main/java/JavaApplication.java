b4cf7e459e70b2380fea44b4441df4a9d1cc0c10
Juraj Benko <juraj.benko@gmail.com>
2024-04-29 10:03:47 +0200

    end of session1, iteration1 in progress

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class JavaApplication {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        //read input
+        System.out.print("Enter text: ");
+        String textInput = console.nextLine();
+
+        //output output
+        System.out.println("Number of words: " + countWords(textInput));
+    }
+
+    public static int countWords(String textInput) {
+        StringTokenizer tokenizer = new StringTokenizer(textInput);
+
+        int wordCount = 0;
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+            if (token.matches("[a-zA-Z]+")) wordCount++;
+        }
+
+        return wordCount;
+    }
 }
--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Test;
+
 public class JavaApplicationTest {
+
+    @Test
+    public void test_it1_mary() {
+        String testedString = "Mary had a little lamb";
+        //countWord
+    }
 }
