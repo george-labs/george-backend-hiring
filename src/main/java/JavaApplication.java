88b4318279efa6d2ad1da60f3e903335da7cb540
Samuel Neceda <69465264+SamuelNeceda@users.noreply.github.com>
2024-05-13 14:05:09 +0200

    iteration2_integration_test_in_progress

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -6,6 +6,17 @@ import wordcount.WordCountService;
 import java.util.Set;
 
 public class JavaApplication {
+
+    private final InputReader inputReader;
+    private final OutputWriter outputWriter;
+    private final WordCountService wordCountService;
+
+    JavaApplication(InputReader inputReader, OutputWriter outputWriter, WordCountService wordCountService) {
+        this.inputReader = inputReader;
+        this.outputWriter = outputWriter;
+        this.wordCountService = wordCountService;
+    }
+
     public static void main(String[] args) {
         StopWordsLoader stopWordsLoader = new StopWordsLoader();
         Set<String> stopWords = stopWordsLoader.loadStopWords("src/main/resources/stopwords.txt");
@@ -14,6 +25,11 @@ public class JavaApplication {
         OutputWriter outputWriter = new OutputWriter();
         WordCountService wordCountService = new WordCountService(stopWords);
 
+        JavaApplication javaApplication = new JavaApplication(inputReader, outputWriter, wordCountService);
+        javaApplication.run();
+    }
+
+    public void run() {
         String sentence = inputReader.getInput();
 
         outputWriter.writeOutput(wordCountService.countWords(sentence));
--- src/test/java/wordcount/WordCountIntegrationTest.java
+++
@@ -1,8 +0,0 @@
-package wordcount;
-
-import org.junit.jupiter.api.Test;
-
-public class WordCountIntegrationTest {
-    // TODO: Will be implemented after other iterations
-
-}
