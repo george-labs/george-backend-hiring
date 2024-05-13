2b1dc9c025e4b6bd40d229c8c6ae91d66ab8eaa2
Samuel Neceda <69465264+SamuelNeceda@users.noreply.github.com>
2024-05-13 14:22:51 +0200

    iteration2_completed_with_integration_test

--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,9 +1,29 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import wordcount.InputReader;
+import wordcount.OutputWriter;
+import wordcount.StopWordsLoader;
+import wordcount.WordCountService;
 
-public class JavaApplicationTest {
-//        @Test
-//    void testWordCountIntegration() {
-//        JavaApplication javaApplication = new JavaApplication();
-//        javaApplication.run();
-//    }
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+class JavaApplicationTest {
+    @Test
+    void testWordCountIntegration() {
+        String inputText = "Mary had a little lamb";
+        InputStream inputStream = new ByteArrayInputStream(inputText.getBytes());
+        System.setIn(inputStream);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        JavaApplication javaApplication = new JavaApplication(new InputReader(), new OutputWriter(), new WordCountService(new StopWordsLoader().loadStopWords("src/main/resources/stopwords.txt")));
+        javaApplication.run();
+
+        String outputText = outputStream.toString();
+        Assertions.assertEquals("Enter text: Number of words: 4", outputText.trim());
+    }
 }
