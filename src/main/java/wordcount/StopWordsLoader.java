1a893fb6d76ad7a83ab5897ddb7c161afee14a11
Samuel Neceda <69465264+SamuelNeceda@users.noreply.github.com>
2024-05-13 13:03:25 +0200

    iteration2_unit_test_in_progress

--- src/main/java/wordcount/StopWordsLoader.java
+++ src/main/java/wordcount/StopWordsLoader.java
@@ -1,6 +1,7 @@
 package wordcount;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
--- src/test/java/wordcount/StopWordsLoaderUnitTest.java
+++ src/test/java/wordcount/StopWordsLoaderUnitTest.java
@@ -3,6 +3,7 @@ package wordcount;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,10 +30,8 @@ class StopWordsLoaderUnitTest {
     void testLoadStopWordsNonExistentFile() {
         StopWordsLoader stopWordsLoader = new StopWordsLoader();
 
-        Set<String> stopWords = stopWordsLoader.loadStopWords("src/main/resources/stopwords_nonexistent.txt");
-        // TODO: catchException
-        assertTrue(stopWords.isEmpty());
+        assertThrows(FileNotFoundException.class, () -> {
+            Set<String> stopWords = stopWordsLoader.loadStopWords("src/main/resources/stopwords_nonexistent.txt");
+        });
     }
-
-
 }
