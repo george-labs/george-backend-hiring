1725c52f699ae2c96f8df93ff748dbd698f26fc5
Patrik Ragan <ragan.patres@gmail.com>
2024-06-25 14:41:38 +0200

    5th assignment

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -5,21 +5,24 @@ public class JavaApplication {
     public static void main(String[] args) {
 
 
-        long result;
+        long totalCount;
+        long distinctCount;
         if (args.length == 1) {
             var textFile = args[0];
             List<String> inputLines = new FileLoader().loadFile(textFile);
-            result = new WordCounter(new FileLoader()).countNotBlackListedWords(WordUtils.splitWords(inputLines));
+            totalCount = new WordCounter(new FileLoader()).countNotBlackListedWords(WordUtils.splitWords(inputLines), false);
+            distinctCount = new WordCounter(new FileLoader()).countNotBlackListedWords(WordUtils.splitWords(inputLines), true);
             //process text file
         } else if (args.length == 0) {
             System.out.print("Enter text: ");
             var input = new InputProcessor(System.in).readInput();
-            result = new WordCounter(new FileLoader()).countNotBlackListedWords(input);
+            totalCount = new WordCounter(new FileLoader()).countNotBlackListedWords(input, false);
+            distinctCount = new WordCounter(new FileLoader()).countNotBlackListedWords(input, true);
         } else {
             throw new UnsupportedOperationException();
         }
 
-        System.out.print("Number of words: " + result);
+        System.out.print("Number of words: " + totalCount + " unique: " + distinctCount);
     }
 
 
