80367cc3fa78e21957ea3438cc486b816cb1ed43
Patrik Ragan <ragan.patres@gmail.com>
2024-06-25 11:25:55 +0200

    1st assignment - tests

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -4,13 +4,7 @@ import java.util.regex.Pattern;
 public class WordCounter {
 
     public long countWords(String input) {
-        int counter = 0;
-//        for (var item : splitInput(input)) {
-////            System.out.println(item);
-//            if (item.matches("[a-zA-Z]+")) {
-//                counter++;
-//            }
-//        }
+
         var pattern = Pattern.compile("[a-zA-Z]+");
 
         return Arrays.stream(splitInput(input))
--- src/test/java/WordCounterTest.java
+++ src/test/java/WordCounterTest.java
@@ -1,18 +1,37 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WordCounterTest {
 
-    @Test
-    void testWordCount(){
-        var result = new WordCounter().countWords("Mary had a little lamb");
-        Assertions.assertEquals(5, result);
+    @ParameterizedTest
+    @MethodSource("inputs")
+    void testWordCount(String input, int expectedCount) {
+        var result = new WordCounter().countWords(input);
+        Assertions.assertEquals(expectedCount, result);
+    }
+
+    private static Stream<Arguments> inputs() {
+        return Stream.of(
+                Arguments.of("Mary had a little lamb", 5),
+                Arguments.of("Mary had a little lamb 7", 5),
+                Arguments.of("Mary had a little   lamb ", 5),
+                Arguments.of("Mary had a little li312tt321le   lamb ", 5),
+                Arguments.of("Mary had a little   lamb !ddsf .dsasd", 5),
+                Arguments.of("4234232 423423", 0),
+                Arguments.of("4234232 .....", 0),
+                Arguments.of("4234232 ..... asdhksadhjagsdhaghasdj k", 2)
+        );
     }
 
     @Test
-    void testREmptyString(){
+    void testEmptyString() {
         var result = new WordCounter().countWords("");
         Assertions.assertEquals(0, result);
     }
