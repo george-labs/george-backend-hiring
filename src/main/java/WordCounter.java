b0f3129f01218928d2f8bbf0e71ce0400bab66e8
Hannes <github@hare.at>
2024-07-16 11:23:37 +0200

    iteration 4 my first idea about it using an argument as return value

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -1,3 +1,4 @@
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
@@ -6,6 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WordCounter {
+	final static Logger logger = Logger.getLogger(WordCounter.class.getName());
 	final Pattern wordPattern = Pattern.compile("([a-zA-Z]+)");
 	final Set<String> stopWords;
 	
@@ -16,16 +18,32 @@ public class WordCounter {
 	}
 
 	public int countWords(final String sentence) {
+		return extractWords(sentence, null);
+	}
+
+	/**
+	 * 
+	 * @param sentence
+	 * @param container the collection can also be null
+	 * @return
+	 */
+	public int extractWords(final String sentence, final Collection<String> container) {
 		final Matcher m = wordPattern.matcher(sentence);
 		int wordsCount = 0;
 		while (m.find()) {
 			final String word = m.group();
 			if (this.stopWords.contains(word)) {
-				Logger.getLogger(WordCounter.class.getName()).log(Level.FINER, "skip {0}", word);
+				logger.log(Level.FINER, "skip {0}", word);
 			} else {
+				if (container != null)
+					container.add(word);
 				++wordsCount;
 			}
 		}
+		if (container == null)
+			logger.log(Level.FINEST, "sentence '{0}' => count {1}", new Object[] {sentence, wordsCount});
+		else
+			logger.log(Level.FINEST, "sentence '{0}' => count {1} container size {1}", new Object[] {sentence, wordsCount, container.size()});
 		return wordsCount;
 	}
 }
