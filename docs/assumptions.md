0cd3ca4cfa8bc9b12ffd16ce0307023cf9945e5d
Hannes <github@hare.at>
2024-07-16 11:01:37 +0200

    iteration 3 reading CLI

--- docs/assumptions.md
+++ docs/assumptions.md
@@ -9,3 +9,7 @@ i.e. "sdf2asdf" will result in 2.
 
 I assume the file stopwords.txt is located in the current working directory.
 I will not care about the exception if the file is not found I pass it over to the caller.
+
+## Iteration 3
+
+without an command line argument it works as before.
--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,13 +1,22 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class JavaApplication {
 	public static void main(final String[] args) throws Exception {
 		final WordCounter wordCounter = new WordCounter(Files.readAllLines(Path.of("stopwords.txt")));
 
-		// read a line and count the number of words inside
-		System.out.print("Enter text: ");
-		final String input = System.console().readLine();
+		final String input;
+		if (args.length == 0) {
+			// read a line and count the number of words inside
+			System.out.print("Enter text: ");
+			input = System.console().readLine();
+		} else {
+			final String filename = args[0];
+			Logger.getLogger(JavaApplication.class.getName()).log(Level.FINER, "using file {0}", filename);
+			input = Files.readString(Path.of(filename));
+		}
 		System.out.printf("Number of words: %d\n", wordCounter.countWords(input));
 	}
 }
