14801dbf97395a8f03872ab24362bc12e4da4b68
f.sukenik <f.sukenik@sportradar.com>
2024-08-01 09:55:57 +0200

    feat: add logic to main method

--- src/main/java/com/george/interview/WordCountApplication.java
+++ src/main/java/com/george/interview/WordCountApplication.java
@@ -1,5 +1,6 @@
 package com.george.interview;
 
+import com.george.interview.counter.WordsCounter;
 import com.george.interview.input.InputReader;
 import java.io.IOException;
 
@@ -8,7 +9,13 @@ public class WordCountApplication {
   public static void main(String[] args) throws IOException {
 
     InputReader reader = new InputReader(System.in);
+    WordsCounter counter = new WordsCounter();
+
+    System.out.print("Enter text: ");
+
     var message = reader.readLine();
-    System.out.println(message);
+    var wordCount = counter.countWords(message);
+
+    System.out.format("Number of words: %d", wordCount);
   }
 }
