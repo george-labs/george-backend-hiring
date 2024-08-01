f17ebd53286b1290014853e49bc84978975149fa
f.sukenik <f.sukenik@sportradar.com>
2024-08-01 13:32:35 +0200

    test: create missing stop words file test

---
+++ src/test/java/com/george/interview/WordsExecutorStopWordsTest.java
@@ -0,0 +1,42 @@
+package com.george.interview;
+
+import com.george.interview.executor.WordsCountExecutor;
+import static com.george.interview.executor.WordsCountExecutor.STOP_WORDS_FILE_NAME;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class WordsExecutorStopWordsTest {
+
+  private WordsCountExecutor executor;
+
+  private ByteArrayOutputStream outputStreamCaptor;
+
+  @BeforeEach
+  void setUp() {
+
+    outputStreamCaptor = new ByteArrayOutputStream();
+    var wordsFile = getClass().getClassLoader().getResourceAsStream("integration-iteration-2");
+    executor = new WordsCountExecutor(wordsFile, new PrintStream(outputStreamCaptor), new String[] {}, STOP_WORDS_FILE_NAME);
+  }
+
+  @Test
+  void nullStopWordsFileDoesNotBreakTheImplementation() {
+
+    var wordsFile = getClass().getClassLoader().getResourceAsStream("integration-iteration-2");
+    executor = new WordsCountExecutor(wordsFile, new PrintStream(outputStreamCaptor), new String[] {}, null);
+    executor.execute();
+    Assertions.assertEquals("Enter text: Number of words: 4, unique: 4", outputStreamCaptor.toString());
+  }
+
+  @Test
+  void notExistingStopWordsFileDoesNotBreakTheImplementation() {
+
+    var wordsFile = getClass().getClassLoader().getResourceAsStream("integration-iteration-2");
+    executor = new WordsCountExecutor(wordsFile, new PrintStream(outputStreamCaptor), new String[] {}, "file-that-does-not-exist");
+    executor.execute();
+    Assertions.assertEquals("Enter text: Number of words: 4, unique: 4", outputStreamCaptor.toString());
+  }
+}
