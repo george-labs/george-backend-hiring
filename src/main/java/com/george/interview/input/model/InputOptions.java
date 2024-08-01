1069c607be484958a29739246777fd8545f81cea
f.sukenik <f.sukenik@sportradar.com>
2024-08-01 14:25:55 +0200

    feat: implement options extraction from one command line arg

--- src/main/java/com/george/interview/input/model/InputOptions.java
+++ src/main/java/com/george/interview/input/model/InputOptions.java
@@ -6,7 +6,21 @@ public record InputOptions(String providedFile, boolean isIndexingRequired) {
 
     public InputOptions buildFromCommandLineArgs(String[] commandLineArgs) {
 
+      if (commandLineArgs == null || commandLineArgs.length == 0) {
+        return new InputOptions(null, false);
+      }
+      if (commandLineArgs.length == 1) {
+        return extractFromOneArg(commandLineArgs[0]);
+      }
       return null;
+    }
+
+    private InputOptions extractFromOneArg(String commandLineArg) {
+
+      if ("-index".equals(commandLineArg)) {
+        return new InputOptions(null, true);
+      }
+      return new InputOptions(commandLineArg, false);
     }
   }
 }
