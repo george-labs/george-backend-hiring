3a718819b9dabb03db8907cd9a308c54a043e90d
Andrii Pyzh <andriy.pyzh2412@gmail.com>
2024-08-05 11:02:13 +0200

    read stopwords file

--- src/main/java/wordcounter/WordCounter.java
+++ src/main/java/wordcounter/WordCounter.java
@@ -1,5 +1,9 @@
 package wordcounter;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class WordCounter {
@@ -17,7 +21,17 @@ public class WordCounter {
     }
 
     private boolean isValidWord(String word) {
-        return word.matches("[a-zA-Z]+");
+        List<String> stopWords = getStopWords();
+        return word.matches("[a-zA-Z]+") && !stopWords.contains(word);
+    }
+
+    private List<String> getStopWords() {
+        Path path = Paths.get("stopwords.txt");
+        try {
+            return Files.readAllLines(path);
+        } catch (IOException ex) {
+            throw new RuntimeException("Error reading stopwords.txt");
+        }
     }
 
 }
