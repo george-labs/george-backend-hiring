a647691e5e415d69525b3bfc960b8f7dfb78686d
Dubinin Oleksandr <alexandr.dubinin@extern.a1.at>
2024-08-19 10:45:48 +0200

    Refactor to use words separator as constant

--- src/main/java/com/geogre/wordcount/WordCount.java
+++ src/main/java/com/geogre/wordcount/WordCount.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 
 public class WordCount {
 
+    private static final String SEPARATOR = " ";
+
     public void start() {
         System.out.print("Please, enter text: ");
         int totalWords = 0;
         Scanner scanner = new Scanner(System.in);
         try {
             String input = scanner.nextLine();
-            String[] words = input.split(" ");
+            String[] words = input.split(SEPARATOR);
             totalWords = words.length;
         } catch (NoSuchElementException ex) {
             System.out.println();
