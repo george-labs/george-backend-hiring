cf010cc9bbf228bcc0095c5b0ab93129b94ab408
Dubinin Oleksandr <alexandr.dubinin@extern.a1.at>
2024-08-19 11:29:06 +0200

    Introduce new test to cover words with digits and special characters

--- src/test/java/com/geogre/wordcount/WordCountTest.java
+++ src/test/java/com/geogre/wordcount/WordCountTest.java
@@ -15,6 +15,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class WordCountTest {
 
     private static final String TEST_INPUT = "The grey goose walks";
+    private static final String TEST_INPUT_MIXED = "Let's GO there !";
+    private static final String TEST_INPUT_MIXED_NO_CLEAR_WORDS = "Let's p1ck em!";
+
     private static final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
     private final WordCount wordCount = new WordCount();
@@ -56,6 +59,30 @@ class WordCountTest {
     @Test
     void shouldStartEmptyString() {
         setUpInput("");
+        wordCount.start();
+        String[] outputParts = outputStream.toString().split(":");
+
+        if (outputParts.length < 3) {
+            fail("The number of parts should be 3: [prompting]: [info]: [number]");
+        }
+        assertEquals(0, Integer.parseInt(outputParts[2].trim()));
+    }
+
+    @Test
+    void shouldStartStringWithDigits() {
+        setUpInput(TEST_INPUT_MIXED);
+        wordCount.start();
+        String[] outputParts = outputStream.toString().split(":");
+
+        if (outputParts.length < 3) {
+            fail("The number of parts should be 3: [prompting]: [info]: [number]");
+        }
+        assertEquals(2, Integer.parseInt(outputParts[2].trim()));
+    }
+
+    @Test
+    void shouldStartStringWithNoClearWords() {
+        setUpInput(TEST_INPUT_MIXED_NO_CLEAR_WORDS);
         wordCount.start();
         String[] outputParts = outputStream.toString().split(":");
 
