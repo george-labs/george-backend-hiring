0e9d5619d2c06e48660d26d8a87b24417d29f09a
Dubinin Oleksandr <alexandr.dubinin@extern.a1.at>
2024-08-19 13:53:24 +0200

    The stopwords file loading was added

--- src/main/java/com/geogre/wordcount/WordCountApplication.java
+++ src/main/java/com/geogre/wordcount/WordCountApplication.java
@@ -2,11 +2,15 @@ package com.geogre.wordcount;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class WordCountApplication {
 
     private static final String SEPARATOR = " ";
+    private static final String STOPWORDS_PATH = "/stopwords.txt";
 
     public static void main(String[] args) {
         configureWordCount().start();
@@ -18,6 +22,11 @@ public class WordCountApplication {
     }
 
     private static @NotNull List<String> getStopWords() {
-        return List.of("the", "a", "of", "on");
+        InputStream inputStream = WordCountApplication.class.getResourceAsStream(STOPWORDS_PATH);
+        if (inputStream == null) {
+            throw new IllegalStateException("Stopwords config is not found");
+        }
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        return bufferedReader.lines().toList();
     }
 }
