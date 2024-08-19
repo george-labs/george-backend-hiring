27c8160307b8adc78311f141c971e4f04b3a4858
Dubinin Oleksandr <alexandr.dubinin@extern.a1.at>
2024-08-19 11:23:35 +0200

    Adapt implementation to fulfill word criteria

--- src/main/java/com/geogre/wordcount/WordCount.java
+++ src/main/java/com/geogre/wordcount/WordCount.java
@@ -1,24 +1,33 @@
 package com.geogre.wordcount;
 
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class WordCount {
 
     private static final String SEPARATOR = " ";
+    private static final Pattern WORDS = Pattern.compile("[a-z,A-Z]+");
 
     public void start() {
         System.out.print("Enter text: ");
-        int totalWords = 0;
+        long totalWords = 0;
         Scanner scanner = new Scanner(System.in);
         try {
             String input = scanner.nextLine();
-            String[] words = input.split(SEPARATOR);
-            totalWords = words.length;
+            if (!isEmpty(input)) {
+                String[] tokens = input.split(SEPARATOR);
+                totalWords = Arrays.stream(tokens).filter(token -> WORDS.matcher(token).matches()).count();
+            }
         } catch (NoSuchElementException ex) {
             System.out.println();
         }
 
         System.out.print("Number of words: " + totalWords);
+    }
+
+    private boolean isEmpty(String input) {
+        return input.trim().isEmpty();
     }
 }
