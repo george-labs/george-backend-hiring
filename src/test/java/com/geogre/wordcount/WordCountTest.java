6c4c56c70fe91b088d4680e2f9eace55116d84b1
Dubinin Oleksandr <alexandr.dubinin@extern.a1.at>
2024-08-19 10:44:32 +0200

    Add unit test

---
+++ src/test/java/com/geogre/wordcount/WordCountTest.java
@@ -0,0 +1,60 @@
+package com.geogre.wordcount;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
+
+import javax.security.sasl.SaslServer;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordCountTest {
+
+    private static final String TEST_INPUT = "The grey goose walks";
+    private static final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    private final WordCount wordCount = new WordCount();
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        outputStream.reset();
+    }
+
+    @Test
+    void shouldStart() {
+        setUpInput(TEST_INPUT);
+        wordCount.start();
+        String[] outputParts = outputStream.toString().split(":");
+
+        if (outputParts.length < 3) {
+            fail("The number of parts should be 3: [prompting]: [info]: [number]");
+        }
+        assertEquals(4, Integer.parseInt(outputParts[2].trim()));
+    }
+
+    @Test
+    void shouldStartEmptyString() {
+        setUpInput("");
+        wordCount.start();
+        String[] outputParts = outputStream.toString().split(":");
+
+        if (outputParts.length < 3) {
+            fail("The number of parts should be 3: [prompting]: [info]: [number]");
+        }
+        assertEquals(0, Integer.parseInt(outputParts[2].trim()));
+    }
+
+    private void setUpInput(String input) {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+    }
+}
