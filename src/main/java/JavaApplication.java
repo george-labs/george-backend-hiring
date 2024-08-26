31eb98795bbc23ca796ee332c7c2fe6398436b59
Michal Stranak <michal.stranak@siemens-healthineers.com>
2024-08-26 09:59:44 +0200

    Iteration #1 first commit

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,20 @@
+import kotlin.text.Regex;
+
+import java.util.Scanner;
+
 public class JavaApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Enter text: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        var regex = new Regex("[a-zA-Z]*");
+        var words = input.split("\\s+");
+
+
+        scanner.close();
+    }
+
 }
--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,2 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class JavaApplicationTest {
+
+    @Test
+    public void dummyTest() {
+        var expectedValue = true;
+        Assertions.assertEquals(true, expectedValue);
+    }
 }
