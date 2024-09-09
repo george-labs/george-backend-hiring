21259a1aad9f2f3d6c87839b0e34fb68f4a691a5
Edmond Dusa <edmond.dusa@gmail.com>
2024-09-09 10:55:54 +0200

    Done with iteration 2

--- src/main/kotlin/iteration1/WordCounter.kt
+++ src/main/kotlin/iteration1/WordCounter.kt
@@ -1,16 +1,28 @@
 package iteration1
 
-class WordCounter {
+class WordCounter() {
 
+    private val stopwords: MutableSet<String> = mutableSetOf()
+
+    init {
+        val wordsText = object {}.javaClass.getResource("/stopwords.txt")?.readText()
+        wordsText?.split("\n")?.forEach(stopwords::add)
+    }
+
+    fun stopWordsCount():Int = stopwords.size
 
     fun countWords(sentence: String?): Int {
         sentence?.let {
             val regex = "\\b[a-zA-Z]+\\b".toRegex()
-            val matches = regex.findAll(sentence)
+            val matches = regex.findAll(sentence).filter {
+                !stopwords.contains(it.value.trim())
+            }
 
             return matches.count()
         }
 
         return 0
     }
+
+
 }
