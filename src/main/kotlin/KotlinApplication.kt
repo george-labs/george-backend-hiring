f43ee13d38aa146fd7eff0a2c6609fc8e481da20
Edmond Dusa <edmond.dusa@gmail.com>
2024-09-09 10:55:54 +0200

    Done with iteration 2

--- src/main/kotlin/KotlinApplication.kt
+++ src/main/kotlin/KotlinApplication.kt
@@ -1,11 +1,37 @@
 import iteration1.WordCounter
+import java.io.File
 
-fun main() {
+fun main(args: Array<String>) {
     val wordCounter = WordCounter()
-    print("Enter text:")
 
-    val input = readlnOrNull()
-    val size = wordCounter.countWords(input)
+    if(args.isNotEmpty()) {
+        val filename = args[0]
+        val contents: String? = readFileContents(filename)
 
+        contents?.let {
+            printOutput(wordCounter.countWords(it))
+        } ?: run {
+            println("Unable to read file $filename")
+        }
+    } else {
+        print("Enter text:")
+
+        val input = readlnOrNull()
+
+        printOutput(wordCounter.countWords(input))
+    }
+}
+
+fun readFileContents(filename: String): String? {
+    return try {
+        val lines = File(filename).readLines()
+
+        lines.joinToString(" ")
+    } catch (e:Exception) {
+        null
+    }
+}
+
+fun printOutput(size:Int) {
     println("Number of words: $size")
 }
--- src/test/kotlin/KotlinApplicationTest.kt
+++ src/test/kotlin/KotlinApplicationTest.kt
@@ -1,2 +1,48 @@
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
 class KotlinApplicationTest {
+
+    @Test
+    fun testingFileArgs() {
+        // Create a stream to capture output
+        val outputStream = ByteArrayOutputStream()
+        val originalOut = System.out
+
+        // Redirect System.out to the new stream
+        System.setOut(PrintStream(outputStream))
+
+        main(arrayOf("/Users/eddie/Desktop/george-backend-hiring/src/main/resources/mytext.txt"))
+
+        // Restore the original System.out
+        System.setOut(originalOut)
+
+        // Verify the output
+        val capturedOutput = outputStream.toString().trim()
+
+        assertEquals("Number of words: 4", capturedOutput)
+    }
+
+    /*
+    @Test
+    fun testingNoFileArgs() {
+        // Create a stream to capture output
+        val outputStream = ByteArrayOutputStream()
+        val originalOut = System.out
+
+        // Redirect System.out to the new stream
+        System.setOut(PrintStream(outputStream))
+
+        main(emptyArray<String>())
+
+        // Restore the original System.out
+        System.setOut(originalOut)
+
+        // Verify the output
+        val capturedOutput = outputStream.toString().trim()
+
+        assertEquals("Number of words: 4", capturedOutput)
+    }*/
 }
