490b4132c8982f1348aabeec2ad606ff34c695b2
bass998 <75537803+bass998@users.noreply.github.com>
2024-10-16 09:54:06 +0200

    Implement main method

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,6 +1,17 @@
+import service.WordCounter;
+
+import java.util.Scanner;
+
 public class JavaApplication {
 
     public static void main (String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter text:");
+
+        String inputText = scanner.nextLine();
+        scanner.close();
+
+        WordCounter wordCounter = new WordCounter();
+        System.out.println("Number of words: " + wordCounter.countWords(inputText));
     }
 }
