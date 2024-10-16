b9fe85a349db1d1f357c3a9e937e127972951059
bass998 <75537803+bass998@users.noreply.github.com>
2024-10-16 11:29:25 +0200

    Update main method with FileStopWordsProvider

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,15 +1,26 @@
 import input.ConsoleInputReader;
+import input.FileStopWordsProvider;
 import input.InputReader;
+import input.StopWordsProvider;
 import output.ConsoleOutputWriter;
 import output.OutputWriter;
 import service.WordCounter;
 
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.Set;
 
 public class JavaApplication {
 
     public static void main (String[] args) {
-        WordCounter wordCounter = new WordCounter(new HashSet<>());
+        StopWordsProvider stopWordsProvider = new FileStopWordsProvider();
+        Set<String> stopWords = new HashSet<>();
+        try {
+            stopWords = stopWordsProvider.getStopWords("src/main/resources/stopwords.txt");
+        } catch (IOException e){
+            System.out.println("Error reading stopwords.txt");
+        }
+        WordCounter wordCounter = new WordCounter(stopWords);
         InputReader inputReader = new ConsoleInputReader();
         OutputWriter outputWriter = new ConsoleOutputWriter();
 
--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,2 +1,41 @@
+import input.ConsoleInputReader;
+import input.FileStopWordsProvider;
+import input.InputReader;
+import input.StopWordsProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import output.ConsoleOutputWriter;
+import output.OutputWriter;
+import service.WordCounter;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class JavaApplicationTest {
+
+    private StopWordsProvider stopWordsProvider;
+    private InputReader inputReader;
+    private OutputWriter outputWriter;
+    private WordCounter wordCounter;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        stopWordsProvider = new FileStopWordsProvider();
+        inputReader = new ConsoleInputReader();
+        outputWriter = new ConsoleOutputWriter();
+        wordCounter = new WordCounter(stopWordsProvider.getStopWords("src/test/resources/correct_stop_words.txt"));
+    }
+
+    @Test
+    public void givenTextInput_whenCountWords_thenReturnCorrectNumberOfWords() {
+        //given
+        String inputText = "Marry had a little lamb";
+
+        //when
+        int numberOfWords = wordCounter.countWords(inputText);
+
+        //then
+        assertEquals(4, numberOfWords);
+    }
 }
