e430efeb9317f7babe6f5c3b8918fc759e246946
Matěj Antoš <matey.antos@gmail.com>
2024-11-08 10:00:49 +0100

    WIP for iteration 1

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class JavaApplication {
+    // Create a Scanner object to read input from the command line
+    Scanner scanner = new Scanner(System.in);
+
+    // Prompt the user for input
+    System.out.print("Enter something: ");
+
+    // Read a line of input from the user
+    String userInput = scanner.nextLine();
+
+    //split according spaces
+    String[] crudeWords = userInput.split("\\s+");
+    //filterout letter words only
+
+
+    // Print the user input
+        System.out.println("You entered: " + userInput);
+
+    // Close the scanner
+        scanner.close();
 }
---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DummyTest {
+
+    @Test
+    void alwaysSuccess() {
+        boolean success = true;
+        assertTrue(success);
+    }
+}
