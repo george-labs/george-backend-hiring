56807691807a5ac318cb2e935c70d1616039e858
oravecv <oravecv@gmail.com>
2024-11-11 10:00:26 +0100

    1st commit

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class JavaApplication {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter text: ");
+        String textLine = scanner.nextLine();
+        //System.out.println("You entered: " + textLine);
+
+        System.out.println("Number of words: " + textLine.split(" ").length);
+    }
 }
--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,2 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class JavaApplicationTest {
+
+    @Test
+    public void dummyTest() {
+        assertEquals(1, 1, "1 should be equal to 1");
+    }
 }
