361d171594a4bed6e065e50197b273ab1d7302ed
oravecv <oravecv@gmail.com>
2024-11-11 12:00:36 +0100

    iteration 3

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -20,16 +22,20 @@ public class JavaApplication {
             throw new RuntimeException("Resource " + STOP_WORDS_FILE + "not found", e);
         }
 
-        System.out.println("Enter text: ");
-        String textLine = scanner.nextLine();
-
+        String textLine;
+        if (args.length > 0) {
+            textLine = getStringFromFile(args[0]);
+        } else {
+            System.out.println("Enter text: ");
+            textLine = scanner.nextLine();
+        }
         int numberOfWords = countNumberOfAlphabeticalWordsExcludingStopWords(textLine, stopWords);
 
         System.out.println("Number of words: " + numberOfWords);
     }
 
     public static int countNumberOfAlphabeticalWordsExcludingStopWords(String textLine, List<String> stopWords) {
-        List<String> wordList = Arrays.asList(textLine.split(" "));
+        List<String> wordList = Arrays.asList(textLine.split("[ \n]"));
         wordList = wordList.stream()
                 .filter(word -> word.matches("[a-zA-Z]+"))
                 .filter(word -> !stopWords.contains(word))
@@ -38,9 +44,23 @@ public class JavaApplication {
         return wordList.size();
     }
 
-    public static List<String> loadWordsFromResourceFile(String resourceFileName) throws IOException, NullPointerException {
+    public static String getStringFromFile(String fileName) {
+        String textLine;
+
+        try {
+            textLine = Files.readString(Path.of(fileName));
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to read file " + fileName, e);
+        }
+
+        return textLine;
+    }
+
+    private static List<String> loadWordsFromResourceFile(String resourceFileName)
+            throws IOException, NullPointerException {
         InputStream resourceInputStream =
                 JavaApplication.class.getClassLoader().getResourceAsStream(resourceFileName);
-        return Arrays.asList(new String(resourceInputStream.readAllBytes()).split("\\n"));
+        return Arrays.asList(new String(resourceInputStream.readAllBytes()).split("\n"));
     }
+
 }
--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,13 +1,32 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class JavaApplicationTest {
 
     @Test
-    public void testCountNumberOfAlphabeticalWords() {
-        String textLine = "Hello World 123abc";
-        int numberOfWords = JavaApplication.countNumberOfAlphabeticalWords(textLine);
+    public void testCountNumberOfAlphabeticalWordsExcludingStopWords() {
+        String textLine = "Hello World of the 123abc";
+        int numberOfWords = JavaApplication.countNumberOfAlphabeticalWordsExcludingStopWords(
+                textLine, List.of("of", "the"));
         assertEquals(2, numberOfWords, "Number of words should be 2");
     }
+
+
+    @Test
+    void testGetStringFromFile() {
+        String text = JavaApplication.getStringFromFile("src/test/resources/mytext.txt");
+        assertEquals("Mary had\na little\nlamb\n", text,
+                "Text should be 'Mary had\na little\nlamb\n'");
+    }
+
+    @Test
+    void testGetStringFromFileNonExistingFile() {
+        assertThrows(RuntimeException.class, () ->
+                JavaApplication.getStringFromFile("src/test/resources/NON_EXISTING_FILE.txt"));
+    }
+
 }
