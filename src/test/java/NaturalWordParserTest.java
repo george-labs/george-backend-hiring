5ee043a6e4dc97fa94c10b12cace87abf3fd9ce4
Amy Kosinova <amko@ciklum.com>
2024-11-13 10:38:20 +0100

    Iteration 1 final: NaturalWordParser

--- src/test/java/NaturalWordParserTest.java
+++ src/test/java/NaturalWordParserTest.java
@@ -3,21 +3,26 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import javax.swing.text.html.parser.Parser;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NaturalWordParserTest {
 
-    private final NaturalWordParser parser = new NaturalWordParser();
+    private final WordParser parser = new NaturalWordParser();
 
     @ParameterizedTest
     @CsvSource({
             "2, ad ad",
             "1, adads",
-            "0, ''"
+            "0, ''",
     })
     void positiveCase(int expectedCount, String input) {
-
         Assertions.assertEquals(expectedCount, parser.countWords(input));
+    }
 
+    @Test
+    void nullCase() {
+        Assertions.assertThrows(IllegalStateException.class, () -> parser.countWords(null));
     }
 }
