3db63bb6cb3aa17561f4c911127b53e75623e27e
Amy Kosinova <amko@ciklum.com>
2024-11-13 11:32:49 +0100

    Iteration 3 final: Application refactored to read file from argument

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,4 +1,11 @@
 import javax.swing.text.html.parser.Parser;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.ClosedFileSystemException;
 import java.util.Scanner;
 
 public class JavaApplication {
@@ -7,16 +14,42 @@ public class JavaApplication {
 
     public static void main(String[] args) {
 
+        int wordCount;
+        if (args.length > 0) {
+            wordCount = parseFile(args);
+        } else {
+            wordCount = parseInput();
+        }
+        System.out.println("Number of words: " + wordCount);
+    }
+
+    private static int parseFile(String[] args) {
+        File inputFile = new File(args[0]);
+
+        if (!inputFile.exists()) {
+            throw new IllegalStateException("File does not exist: " + args[0]);
+        }
+
+        int wordCount = 0;
+        try (Scanner myReader = new Scanner(inputFile)) {
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                wordCount += parser.countWords(data);
+            }
+        } catch (FileNotFoundException e) {
+            throw new IllegalStateException(e);
+        }
+        return wordCount;
+    }
+
+    private static int parseInput() {
         Scanner in = new Scanner(System.in);
 
         System.out.print("Enter text: ");
         String s = in.nextLine();
 
         int countWords = parser.countWords(s);
-
-
-        System.out.println("Number of words: " + countWords);
+        return countWords;
     }
-
 
 }
--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,3 +1,20 @@
-public class JavaApplicationTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
+class JavaApplicationTest {
+
+
+    @Test
+    void shouldParseFile() {
+        Assertions.assertDoesNotThrow(
+                () -> JavaApplication.main(new String[]{"build/resources/test/testInput.txt"}));
+    }
+
+    @Test
+    void shouldReportMissingFile() {
+        Assertions.assertThrows(IllegalStateException.class,
+                () -> JavaApplication.main(new String[]{"wrong"}));
+    }
 }
