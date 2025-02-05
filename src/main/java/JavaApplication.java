0a1c0d5ce8a9e6561280e4498422146288c82a05
andrej <andrej@DESKTOP-QTUOFPD>
2025-02-05 10:05:20 +0100

    iteration 1

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,4 +1,36 @@
+import java.util.Scanner;
 
 public class JavaApplication {
 
+	public static int getWordsCount(String s) {
+
+		if (s == null)
+			return 0;
+
+		int res = 0;
+
+		String[] ss = s.split(" ");
+		for_tmp: for (String tmp : ss) {
+			if (tmp.trim().length() == 0)
+				continue;
+			for (char c : tmp.toCharArray()) {
+				if (!(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')) {
+					continue for_tmp;
+				}
+			}
+			res++;
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+
+		System.out.print("Enter text: ");
+		String s = null;
+
+		try (Scanner scanner = new Scanner(System.in);) {
+			s = scanner.nextLine();
+		}
+		System.out.println(getWordsCount(s));
+	}
 }
--- src/test/java/JavaApplicationTest.java
+++ src/test/java/JavaApplicationTest.java
@@ -1,2 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+import org.junit.jupiter.api.Test;
+
 public class JavaApplicationTest {
+
+	@Test
+	public void testDummy() {
+		assertTrue(true);
+	}
 }
