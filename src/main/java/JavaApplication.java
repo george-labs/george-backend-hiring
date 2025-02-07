9862f7915d143f693430bf5d6e126e82d7850c8d
alexey <alexeybezglasnyy@gmail.com>
2025-02-07 09:59:44 +0100

    Session 1: input & output from cl

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 
 public class JavaApplication {
 
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("Enter text: ");
+        String s1 = SCANNER.nextLine();
+        System.out.printf("Number of words: %d%n", countWords(s1));
+    }
+
+    private static int countWords(String inputLine) {
+
+        var splitedString = inputLine.split(" ");
+
+        return 0;
+    }
 }
