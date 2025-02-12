a0b9ae7aea839c045fa5f0df14901f154ef2279e
Dusan Gemza <dusan.gemza@visma.com>
2025-02-12 09:58:03 +0100

    init commit

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,4 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class JavaApplication {
 
+    public static void main(String[] args) {
+
+        System.out.print("Enter the text: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            String sentence = reader.readLine();
+            Arrays.stream(sentence.split(" ")).filter(w)
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
---
+++ src/main/java/utils/StringSplitter.java
@@ -0,0 +1,10 @@
+package utils;
+
+import java.util.Collection;
+
+public class StringSplitter {
+
+    public static Collection<String> splitBy(String text, String splitter) {
+
+    }
+}
