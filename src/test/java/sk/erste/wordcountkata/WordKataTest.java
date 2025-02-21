e20e36452a364b94044f65198025a3db5b13cfae
miromajdan <majdan@myrapps.com>
2025-02-21 13:08:42 +0100

    Session 4: Iteration 3: Word Count 3

--- src/test/java/sk/erste/wordcountkata/WordKataTest.java
+++ src/test/java/sk/erste/wordcountkata/WordKataTest.java
@@ -2,6 +2,7 @@ package sk.erste.wordcountkata;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class WordKataTest {
 
     @Test
-    public void testProcess() throws FileNotFoundException {
+    public void testProcess_fromFile() throws FileNotFoundException {
         WordKata wordKata = new WordKata(new InputTextReader("mytext_test.txt"));
         assertEquals(4, wordKata.countWords());
     }
@@ -25,5 +26,12 @@ public class WordKataTest {
     public void testProcess_whenFileNotExist() {
         WordKata wordKata = new WordKata(new InputTextReader("mytext_test_missing.txt"));
         assertThrows(FileNotFoundException.class, wordKata::countWords);
+    }
+
+    @Test
+    public void testProcess_fromConsole() throws FileNotFoundException {
+        System.setIn(new ByteArrayInputStream("Mary had a little lamb".getBytes()));
+        WordKata wordKata = new WordKata(new InputTextReader(null));
+        assertEquals(4, wordKata.countWords());
     }
 }
