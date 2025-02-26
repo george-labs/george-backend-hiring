a3b2b7cd8a8dceccd9df52ac7279f6fd2e838157
Dusan Bartos <doodeec@gmail.com>
2025-02-26 10:16:38 +0100

    KATA-1 add counter unit test

--- docs/assumptions.md
+++ docs/assumptions.md
@@ -2,7 +2,7 @@
 
 ## Iteration 1
 
-Any word containing numbers or non-alphabetical characters is not counted
-- i.e. `l1ttle` is not counted
+Any word containing numbers or non-alphabetical characters is not counted, i.e.:
+- `l1ttle` is not counted
 - `Mary, had` is one word, because punctuation is not allowed at this point
 - `Mary had a lamb.` is 3 words, because of the same reason
---
+++ src/test/kotlin/com/dusanbartos/wordcount/domain/WordCounterTest.kt
@@ -0,0 +1,55 @@
+package com.dusanbartos.wordcount.domain
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class WordCounterTest {
+
+    private lateinit var counter: WordCounter
+
+    @BeforeEach
+    fun setup() {
+        counter = WordCounter()
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "Mary had a little lamb; 5",
+        "Mary had a little; 4",
+        "Lorem ipsum dolor sit amet; 5",
+        "Lorem; 1",
+        "Loooooooooooooorem iiiiipppppsssssssssssssuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuum; 2",
+        delimiter = ';'
+    )
+    fun `only valid alphabetical characters in words`(sentence: String, expected: Int) {
+        val result = counter.countAll(sentence)
+        Assertions.assertEquals(expected, result) { "Unexpected word count" }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "Mary had a l1ttle lamb; 4",
+        "Mary had, a little!; 2",
+        "Lo_rem ip*sum dolor, si't amet.; 0",
+        delimiter = ';'
+    )
+    fun `invalid characters in words`(sentence: String, expected: Int) {
+        val result = counter.countAll(sentence)
+        Assertions.assertEquals(expected, result) { "Unexpected word count" }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "'   Mary'; 1",
+        "' a  b .../ c.  ,'; 2",
+        "' '; 0",
+        "'         '; 0",
+        delimiter = ';'
+    )
+    fun `invalid input`(sentence: String, expected: Int) {
+        val result = counter.countAll(sentence)
+        Assertions.assertEquals(expected, result) { "Unexpected word count" }
+    }
+}
