2e28701de4498924787a98b4c7d2bf70068bbba1
lengeligor <lengel.igor@gmail.com>
2025-03-03 10:01:53 +0100

    feature/igor_lengel_session_1_iteration_2_ladi - adding stopWords method to WordCounter

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -1,12 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class WordCounter {
+
+    public static final String STOPWORD_FILE_PATH = "stopwords.txt";
+    public static final String STOPWORD_TXT_WAS_NOT_FOUND = "STOPWORD.TXT WAS NOT FOUND!";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter text: ");
         String line = sc.nextLine();
         System.out.println("Number of words: " + count(line));
+    }
+
+    public static List<String> getStopWords(){
+        List<String> stopwords = new LinkedList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(STOPWORD_FILE_PATH));
+            String line = br.readLine();
+
+            while (line != null) {
+
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(STOPWORD_TXT_WAS_NOT_FOUND);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return stopwords;
     }
 
     public static int count(String line) {
