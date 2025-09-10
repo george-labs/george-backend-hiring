d7940057bc35640d336516ed2b411b2e0fb42982
Costas Apostolou <costas.apostolou@qitasc.com>
2025-09-10 13:54:19 +0200

    add method to WordCounter to count the unique words and add the corresponding unit tests

--- src/main/kotlin/WordCounter.kt
+++ src/main/kotlin/WordCounter.kt
@@ -1,5 +1,12 @@
 class WordCounter(private val stopwords: List<String>) {
     fun countWords(words: List<String>): Int {
-        return words.filterNot { stopwords.contains(it.lowercase()) }.size
+        return words.filterStopWords().size
     }
+
+    fun countUniqueWords(words: List<String>): Int {
+        return words.filterStopWords().toSet().size
+    }
+
+    private fun List<String>.filterStopWords(): List<String> =
+        filterNot { stopwords.contains(it.lowercase()) }
 }
--- src/test/kotlin/WordCounterTest.kt
+++ src/test/kotlin/WordCounterTest.kt
@@ -1,36 +1,53 @@
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class WordCounterTest {
     private val counter = WordCounter(listOf("the", "a", "on", "off"))
 
-    @Test
-    fun `returns 0, when input list is empty`() {
-        assertEquals(0, counter.countWords(emptyList()))
+    @Nested
+    inner class WordCountTest {
+        @Test
+        fun `returns 0, when input list is empty`() {
+            assertEquals(0, counter.countWords(emptyList()))
+        }
+
+        @Test
+        fun `returns the number of words contained in the input list`() {
+            assertEquals(3, counter.countWords(listOf("one", "two", "three")))
+        }
+
+        @Test
+        fun `returns the number of valid words contained in the input list`() {
+            assertEquals(3, counter.countWords(listOf("one", "two", "three", "a", "the")))
+        }
+
+        @Test
+        fun `returns 0, when input only contains stop words`() {
+            assertEquals(0, counter.countWords(listOf("a", "the", "on", "off")))
+        }
+
+        @Test
+        fun `mixed case stop words are also filtered out`() {
+            assertEquals(0, counter.countWords(listOf("a", "The", "oN", "oFf")))
+        }
+
+        @Test
+        fun `uppercase stop words are also filtered out`() {
+            assertEquals(0, counter.countWords(listOf("A", "THE", "ON", "OFF")))
+        }
     }
 
-    @Test
-    fun `returns the number of words contained in the input list`() {
-        assertEquals(3, counter.countWords(listOf("one", "two", "three")))
-    }
+    @Nested
+    inner class UniqueWordCountTest {
+        @Test
+        fun `returns the count of unique words, when input contains repeated words`(){
+            assertEquals(1, counter.countUniqueWords(listOf("one", "one")))
+        }
 
-    @Test
-    fun `returns the number of valid words contained in the input list`() {
-        assertEquals(3, counter.countWords(listOf("one", "two", "three", "a", "the")))
-    }
-
-    @Test
-    fun `returns 0, when input only contains stop words`() {
-        assertEquals(0, counter.countWords(listOf("a", "the", "on", "off")))
-    }
-
-    @Test
-    fun `mixed case stop words are also filtered out`() {
-        assertEquals(0, counter.countWords(listOf("a", "The", "oN", "oFf")))
-    }
-
-    @Test
-    fun `uppercase stop words are also filtered out`() {
-        assertEquals(0, counter.countWords(listOf("A", "THE", "ON", "OFF")))
+        @Test
+        fun `returns the count of unique words, when input contains repeated words and stop words`(){
+            assertEquals(1, counter.countUniqueWords(listOf("one", "one", "a")))
+        }
     }
 }
