1f0fa159004817f1f1b9fb562b26a841924af2ae
Costas Apostolou <costas.apostolou@qitasc.com>
2025-09-10 10:27:43 +0200

    implement the iteration 1 requirements in the main method

--- src/main/kotlin/KotlinApplication.kt
+++ src/main/kotlin/KotlinApplication.kt
@@ -1,4 +1,8 @@
 fun main() {
-
+    val input = readInput()
+    val words = parseInput(input)
+    val count = countWords(words)
+    val message = constructMessage(count)
+    printMessage(message)
 }
 
