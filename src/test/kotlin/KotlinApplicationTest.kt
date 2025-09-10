ced497ab1a5bc66afc50618d186592b4ec15df3c
Costas Apostolou <costas.apostolou@qitasc.com>
2025-09-10 09:43:16 +0200

    implement reading input from arbitrary input stream and unit test it

--- src/test/kotlin/KotlinApplicationTest.kt
+++ src/test/kotlin/KotlinApplicationTest.kt
@@ -1,2 +1,25 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+
 class KotlinApplicationTest {
+
+    private val bytearrayOutStream = ByteArrayOutputStream()
+
+    @Test
+    fun `returns the input that the user provided`() {
+        val input = "Mary had a little lamb"
+
+        assertEquals(input, readInput(bytearrayOutStream) { input })
+        assertEquals("Enter text: ", String(bytearrayOutStream.toByteArray()))
+
+    }
+
+    @Test
+    fun `can support an empty string input`() {
+        assertTrue(readInput(bytearrayOutStream) { "" }.isEmpty())
+        assertEquals("Enter text: ", String(bytearrayOutStream.toByteArray()))
+
+    }
 }
