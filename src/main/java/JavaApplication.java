53f3b6f02b21614b623d782641d02828218df871
Antonin Klein <antonin.klein@gmail.com>
2025-09-19 14:03:28 +0200

    4th session

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -13,13 +13,30 @@ import java.util.Scanner;
 public class JavaApplication {
 
     public static void main(String... args) {
+        List<String> stopwords = readStopwords();
+        String words;
+        if (args.length > 0) {
+            words = wordsFromFile(args[0]);
+        } else {
+            words = wordsFromConsole();
+        }
+        System.out.println("Number of words:" + new WordCounter().countWords(words, stopwords));
+    }
+
+    private static String wordsFromFile(String path) {
+        try {
+            return Files.readString(Path.of(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String wordsFromConsole() {
         // "wor3d word" = per regex in description will be split to wor, d, word
 
         System.out.println("Enter text:");
         Scanner scan = new Scanner(System.in);
-
-        List<String> stopwords = readStopwords();
-        System.out.println("Number of words:" + new WordCounter().countWords(scan.nextLine(), stopwords));
+        return scan.nextLine();
     }
 
     public static List<String> readStopwords() {
