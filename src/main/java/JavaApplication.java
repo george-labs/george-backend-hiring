84e1782deaeca4c5b7352145d69f943b7ebb82e1
Petro Khovrashchuk <petro.khovrashchuk@gmail.com>
2025-09-30 09:52:00 +0200

    add initial implementation

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
 
 public class JavaApplication {
+
+    private static final String ENTER_TEXT = "Enter text: ";
+    private static final String NUMBER_OF_WORDS = "Number of words: ";
+    private static final String SPACE = " ";
+
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        System.out.print(ENTER_TEXT);
+
+        final String text = scanner.nextLine();
+        final int wordCount = countWords(text);
+
+        System.out.println(NUMBER_OF_WORDS + wordCount);
+    }
+
+    private static int countWords(final String text) {
+        return text.split(SPACE).length;
+    }
 
 }
