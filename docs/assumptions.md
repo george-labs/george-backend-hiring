9ba997d90762f0dba09122b2486634b67a657f2a
kursadcayir <omerkursadcayir@gmail.com>
2025-10-21 09:59:47 +0200

    end of first iteration

--- docs/assumptions.md
+++ docs/assumptions.md
@@ -1,3 +1,6 @@
 # Assumptions per iteration
 
 ## Iteration 1
+
+As a requirement we are exepecting stretches of letters (a-z,A-Z)
+while we are seperating text to words we will use nonalphabetic as seperator
--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
 
 public class JavaApplication {
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("Enter text: ");
+        String userInput = scan.nextLine();
+
+        int wordCount = getWordCount(userInput);
+
+        System.out.println("Number of words: " + wordCount);
+    }
+
+    private static int getWordCount(String userInput) {
+
+        return 5;
+
+
+    }
 }
