60567ccc14a1629448d6de7ab1cbc2d3ab696aef
Milos Vnucko <milosz.milosz@gmail.com>
2025-10-28 10:17:51 +0100

    added edge case test and added assumption for iteration 1

--- docs/assumptions.md
+++ docs/assumptions.md
@@ -1,3 +1,6 @@
 # Assumptions per iteration
 
 ## Iteration 1
+The assumption is that user will enter only words without any special characters or numbers.
+
+## Iteration 2
--- src/test/kotlin/KotlinApplicationTest.kt
+++ src/test/kotlin/KotlinApplicationTest.kt
@@ -6,4 +6,10 @@ class KotlinApplicationTest {
         val noOfWords = countWords("Mary had a little lamb")
         assert(noOfWords == 5)
     }
+
+    @Test
+    fun testCountWordsEmpty() {
+        val noOfWords = countWords("")
+        assert(noOfWords == 0)
+    }
 }
