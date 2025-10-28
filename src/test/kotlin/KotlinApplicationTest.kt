6654fa9ad09b2e22a099001a05fd3b5bdc66f4cc
Milos Vnucko <milosz.milosz@gmail.com>
2025-10-28 11:13:41 +0100

    Added read file edge cases

--- src/test/kotlin/KotlinApplicationTest.kt
+++ src/test/kotlin/KotlinApplicationTest.kt
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.io.FileNotFoundException
 
 class KotlinApplicationTest {
     @Test
@@ -18,5 +20,25 @@ class KotlinApplicationTest {
         val stopWordsText = getStopWords().joinToString(" ")
         val noOfWords = countWords(stopWordsText)
         assert(noOfWords == 0)
+    }
+
+    @Test
+    fun testReadFile() {
+        val content = readFile("src/main/resources/stopwords.txt")
+        assert(content.isNotEmpty())
+    }
+
+    @Test
+    fun testReadFileException() {
+        assertThrows<FileNotFoundException> {
+            readFile("src/main/resources/stopwords123.txt")
+        }
+    }
+
+    @Test
+    fun testReadFileEmpty() {
+        assertThrows<FileNotFoundException> {
+            readFile("")
+        }
     }
 }
