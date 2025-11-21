b2125a39a61b3e5fa53f47448a8c42d816e600e0
Boris Schegolev <a-boris.schegolev@adtest.mpsv.cz>
2025-11-21 10:23:53 +0100

    Iteration 1 Session 2
     - comments added

--- src/main/kotlin/WordService.kt
+++ src/main/kotlin/WordService.kt
@@ -1,11 +1,17 @@
 class WordService {
     fun countWords(input: String): Int {
+
+        // Check empty values
         val trimmed = input.trim()
         if (trimmed.isEmpty()) {
             return 0
         }
+
+        // Split
         val chunks = trimmed.split(Regex("[^a-zA-Z]+"))
+            // Skip empty chunks
             .filter {s: String -> s.isNotEmpty() }
+
         return chunks.size
     }
 }
