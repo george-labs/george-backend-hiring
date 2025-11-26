11e202529c12480bf74270b361f7a3566ee141d1
Igor Vasicek <igor.vasicek.dev@gmail.com>
2025-11-26 09:02:39 +0100

    Updated tests for iteration 1 by extracting into word extensions

--- src/test/kotlin/KotlinApplicationTest.kt
+++ src/test/kotlin/KotlinApplicationTest.kt
@@ -1,8 +1,25 @@
+import extensions.countWords
+import extensions.getDividedWords
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class KotlinApplicationTest {
+    val testWords="Mary had a little lamb"
+
+    val notWords="123 )(* +| 45 !"
+
     @Test
     fun dummyTest(){
         assert("dummy".isNotBlank())
+    }
+
+    @Test
+    fun testWordCount(){
+        assertEquals(5, testWords.getDividedWords().countWords())
+    }
+
+    @Test
+    fun testNotWordCount(){
+        assertEquals(0, notWords.getDividedWords().countWords())
     }
 }
