5274edc4699eb746550f45f6a902f5b1602bfb54
sergei.hapugin <sergei.hapughin@gmail.com>
2026-01-13 09:17:06 +0100

    Add unit test to verify main function does not throw exceptions

--- src/test/kotlin/KotlinApplicationTest.kt
+++ src/test/kotlin/KotlinApplicationTest.kt
@@ -1,2 +1,9 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+
 class KotlinApplicationTest {
+    @Test
+    fun `main should not throw any exceptions`() {
+        assertDoesNotThrow { main() }
+    }
 }
